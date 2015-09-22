059cf0fa03039ca2f8c46955a9f3021b3f43191d
Ian Candy <iancandysemail@gmail.com>
2015-09-22 17:34:28 -0400

    update validates syntax

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
 
-  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
 
   validate :neighborhood_exists
 
