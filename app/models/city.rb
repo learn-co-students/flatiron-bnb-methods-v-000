9723cb3808a4b0fe899ff36fa744a43f3219316f
Blake Johnson <blake41@gmail.com>
2015-10-01 15:19:57 -0400

    Update city.rb

--- app/models/city.rb
+++ app/models/city.rb
@@ -5,7 +5,7 @@ class City < ActiveRecord::Base
   
   # Returns all of the available apartments in a city, given the date range
   def city_openings(start_date, end_date)
-    booked_listings = "select distinct(l.id) FROM cities c JOIN neighborhoods n on c.id = n.city_id JOIN listings l on n.id = l.neighborhood_id JOIN reservations r on l.id = r.listing_id WHERE r.checkin >= '2014-01-08' AND r.checkout <= '2014-05-21'"
+    booked_listings = "select distinct(l.id) FROM cities c JOIN neighborhoods n on c.id = n.city_id JOIN listings l on n.id = l.neighborhood_id JOIN reservations r on l.id = r.listing_id WHERE r.checkin >= #{start_date} AND r.checkout <= #{end_date}"
     Listing.find_by_sql("select l.id, l.title FROM cities c JOIN neighborhoods n on c.id = n.city_id JOIN listings l on n.id = l.neighborhood_id WHERE l.id NOT IN (#{booked_listings})")
   end
 
