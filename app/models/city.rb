6951ca7abee47898fda8b62b4783daf5f491d1c6
jakebrady5 <jakebrady5@gmail.com>
2016-01-15 23:01:30 -0500

    progress

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,13 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings
+  end
 
+  def self.highest_ratio_res_to_listings
+  end
+
+  def self.most_res
+  end
+
+end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,10 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  def duration
+  end
+
+  def total_price
+  end
+
 end
