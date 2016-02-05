89b9a13ea35bc8b6b3a18544becfb2fa09f3bca2
Shana L Moore <shana.lavina.moore@gmail.com>
2016-02-05 07:43:26 -0800

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
+  has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
+  has_many :reviews, :foreign_key => 'guest_id'
+  has_many :guests, through: :reservations, foreign_key: 'guest_id'
+  has_many :guest_listings, through: :trips, source: :listing
+  has_many :hosts, through: :guest_listings, foreign_key: 'host_id'
+  has_many :host_reviews, through: :listings, source: :reviews
   
 end
