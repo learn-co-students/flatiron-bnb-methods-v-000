c9179043f2d87c646615548fc123e7287bf9fed8
Brett <brettrheenan@gmail.com>
2016-02-15 00:05:07 +0200

    added user association

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,12 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
+  belongs_to :host, :class_name => "User"
   has_one :review
-  validates :checkin, :checkout, presence: true
+  validates :checkin, presence: true
+  validates :checkout, presence: true
   validate :cant_make_reservation_on_own_listing
-  # after_validation :reservation_conflict
+  # validate :reservation_conflict
 
 
   def duration
@@ -12,13 +14,19 @@ class Reservation < ActiveRecord::Base
   end
 
   
-
-  # def reservation_conflict
-  # 	conflict = Reservation.all.detect do |reservation|
-  # 		checkin == reservation.checkin || checkout == reservation.checkout
+  # def checkin_conflict
+  # 	if !listing.nil? && listing.reservations.include?(checkin)
+  # 		false
   # 	end
-  # 	if !conflict.nil?
-  # 		errors.add(:conflict, "Your reservation conlicts with someone else's")
+  # end
+  # def reservation_conflict
+  # 	if !listing.nil?
+  # 		conflict = listing.reservations.detect do |reservation|
+  # 			checkin == reservation.checkin || checkout == reservation.checkout
+  # 		end
+  # 		if !conflict.nil?
+  # 			errors.add(:conflict, "Your reservation conlicts with someone else's")
+  # 		end
   # 	end
   # end	
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
+  has_many :guests, :foreign_key => 'host_id', through: :listings
+  has_many :hosts, :foreign_key => 'guest_id', through: :listings
   # has_many :guests
   
 end
