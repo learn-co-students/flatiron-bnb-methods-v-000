dbc136ed8371f666267f2c3920de765382d5cc46
c1505 <coreym247@gmail.com>
2016-02-14 14:42:58 -0500

    completed features for listing spec

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,30 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  before_create :host_status
+
+  after_destroy :false_if_empty
+
+  def host_status
+    self.host.host = true
+    self.host.save
+  end
+
+  def false_if_empty
+    if self.host.listings.empty?
+      self.host.host = false
+      self.host.save 
+    end
+  end
+
+  def average_review_rating
+    rating_array = self.reviews.map {|review| review.rating}
+    total = rating_array.reduce(:+).to_f
+    count = rating_array.count.to_f
+    total / count
+  end
+
+
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -141,6 +141,8 @@ describe Listing do
           price: "150.00",
           neighborhood: santa_monica,
           host: user)
+      # binding.pry
+        
         expect(user.reload.host?).to eq(true)
       end
     end
