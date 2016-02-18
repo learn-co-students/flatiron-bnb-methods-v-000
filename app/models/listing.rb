25ae6f0ed441496136cdb28d9e2dfff8e54b5232
lisamarie616 <lisamarie616@gmail.com>
2016-02-18 10:16:54 -0800

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,36 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+
+  before_save :make_host
+  before_destroy :remove_host
+
+  def booked?(start_date, end_date)
+    parsed_start_date = start_date.class == String ? Date.parse(start_date) : start_date
+    parsed_end_date = end_date.class == String ? Date.parse(end_date) : end_date
+    reservations.any? do |reservation|
+      parsed_start_date < reservation.checkout && reservation.checkin < parsed_end_date
+    end
+  end
+
+  def average_review_rating
+    if reviews.count > 0
+      reviews.collect {|review| review.rating}.sum.to_f / reviews.count
+    else
+      "cannot divide by 0"
+    end
+  end
+
+  private
+    def make_host
+      self.host.update(host: true) unless self.host.host
+    end
+
+    def remove_host
+      if self.host.host && self.host.listings.count <= 1
+        self.host.update(host: false)
+      end
+    end
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,13 @@
 class Neighborhood < ActiveRecord::Base
+  include Stats::InstanceMethods
+  extend Stats::ClassMethods
+
   belongs_to :city
   has_many :listings
+  has_many :reservations, through: :listings
+
+  def neighborhood_openings(start_date, end_date)
+    listings.select {|listing| !listing.booked?(start_date, end_date)}
+  end
 
 end
