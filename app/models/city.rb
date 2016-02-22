f4683b75859d339beb2f78ff72516c1316bb8e6b
eric-an <erican@me.com>
2016-02-21 19:50:25 -0800

    create model associations, validations, and methods

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,54 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
-  has_many :listings, :through => :neighborhoods
+  has_many :listings, through: :neighborhoods
+  has_many :reservations, through: :listings
 
+  def city_openings(starting, ending)
+    starting_date = Date.parse(starting)
+    ending_date = Date.parse(ending)
+    available_listings = []
+
+    listings.each do |listing|
+      unavailable = listing.reservations.any? do |reservation|
+        starting_date.between?(reservation.checkin, reservation.checkout) || ending_date.between?(reservation.checkin, reservation.checkout)
+      end
+      unless unavailable
+        available_listings << listing
+      end
+    end
+    return available_listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    popular_city = "There is no popular city."
+    popularity_ratio = 0.00
+    self.all.each do |city|
+      denominator = city.listings.count
+      numerator = city.reservations.count
+      if numerator == 0 || denominator == 0
+        return popular_city
+      else
+        city_ratio = numerator / denominator
+        if city_ratio > popularity_ratio
+          popularity_ratio = city_ratio
+          popular_city = city
+        end
+      end
+    end
+    popular_city
+  end
+
+  def self.most_res
+    most_reservations_city = "Not available."
+    reservation_count = 0
+    self.all.each do |city|
+      reservations = city.reservations.count
+      if reservations > reservation_count
+        reservation_count = reservations
+        most_reservations_city = city
+      end
+    end
+    most_reservations_city
+  end
 end
 
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,54 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  has_many :reservations, through: :listings
 
+  def neighborhood_openings(starting, ending)
+    starting_date = Date.parse(starting)
+    ending_date = Date.parse(ending)
+    available_listings = []
+
+    listings.each do |listing|
+      unavailable = listing.reservations.any? do |reservation|
+        starting_date.between?(reservation.checkin, reservation.checkout) || ending_date.between?(reservation.checkin, reservation.checkout)
+      end
+      unless unavailable
+        available_listings << listing
+      end
+    end
+    return available_listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    most_popular_neighborhood = "None exist."
+    highest_ratio = 0.00
+    
+    self.all.each do |neighborhood|
+      numerator = neighborhood.reservations.count
+      denominator = neighborhood.listings.count
+      if numerator == 0 || denominator == 0
+        "Ratio not available."
+      else
+        neighborhood_ratio = numerator / denominator
+        if neighborhood_ratio > highest_ratio
+          highest_ratio = neighborhood_ratio
+          most_popular_neighborhood = neighborhood
+        end
+      end
+    end
+    most_popular_neighborhood
+  end
+
+  def self.most_res
+    reservations = 0
+    most_reservations = "None exist."
+    self.all.each do |neighborhood|
+      rsvp = neighborhood.reservations.count
+      if rsvp > reservations
+        reservations = rsvp
+        most_reservations = neighborhood
+      end
+    end
+    most_reservations
+  end
 end
