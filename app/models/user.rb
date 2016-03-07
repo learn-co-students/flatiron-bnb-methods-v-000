85e87fef9482907333eacfc02c12ced1e31fecb7
Karuna <imkaruna@gmail.com>
2016-03-07 00:26:50 -0500

    User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,17 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+
+  def guests
+    guest = self.listings.collect {|d| User.find(Reservation.find_by(listing_id: Listing.find_by(host_id: d.id)).guest_id)}
+  end
+
+  def hosts
+    host = self.trips.collect {|t| User.find(Listing.find(Reservation.find_by(guest_id: self.id).listing_id).host_id)}
+  end
+
+  def host_reviews
+    my_reviews = self.guests.collect {|rev| Review.find_by(guest_id: rev.id) }
+  end
 end
