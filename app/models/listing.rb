87f17134a50a82fdd69bf0fa1f2cab9da342ed1d
Karuna <imkaruna@gmail.com>
2016-03-06 22:09:14 -0500

    Listing Model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,32 @@
 class Listing < ActiveRecord::Base
+  #associations
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
-end
+  #validations
+  validates :address, :listing_type, :title, :description,:price,:neighborhood, presence: true
+  #callbacks
+  after_create :change_host_status
+  after_destroy :change_host_status
+
+  def average_review_rating
+    collection_of_ratings = self.reviews.collect {|r| r.rating}
+    (collection_of_ratings.sum)/(collection_of_ratings.count).to_f
+  end
+
+  private
+  def change_host_status
+    #
+    theuser = User.find(self.host_id)
+    if theuser.listings.empty?
+        theuser.host = false
+        theuser.save
+      else
+        theuser.host = true
+        theuser.save
+      end
+
+    end
+  end
