c0d43e69971f4c3594edddfb7bb550aa750c2dc1
pajamaw <pjpeterww@gmail.com>
2016-03-07 14:56:18 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,15 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+  validate :trip_occured?
+
+  def trip_occured?
+    if !reservation.nil? && reservation.status == "accepted" && reservation.checkout < Date.today
+      true
+    else
+      errors.add(:trip_occured?, "Please wait until the end of the trip to write a review.")
+    end
+  end
 end
