36ae9059aecf771ff250351d2585498e8310fb58
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-07 22:46:20 -0700

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,32 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  #validates :address, presence: true
+  #validates :listing_type, presence: true
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
+
+  before_save :set_host
+  after_destroy :set_user
+
+  def average_review_rating
+    review_ratings = []
+      self.reservations.each do |reservation|
+        review_ratings << reservation.review.rating
+      end
+      review_ratings.sum.to_f / review_ratings.count
+    end
+
+
+  private 
+
+    def set_host 
+      self.host.update(host: true)
+    end
+
+    def set_user
+      if self.host.listings.empty?
+        self.host.update(host: false)
+      end
+    end
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,55 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(start_date, end_date)
+    available_listings = []
+    self.listings.each do |listing|
+      conflicts = []
+      listing.reservations.each do |reservation|
+        conflicts = []
+        if reservation.checkin <= start_date.to_date && reservation.checkout > start_date.to_date
+          conflicts << reservation
+        end
+      end
+      if !conflicts.any?
+        available_listings << listing
+      end
+    end
+    available_listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    highest_city = ""
+    highest_ratio = ""
+    self.all.each do |city|
+      city.listings.each do |listing|
+        if listing.reservations.count > highest_ratio.to_i
+          highest_ratio = listing.reservations.count
+          highest_city = city
+        end
+
+      end
+    end
+    highest_city
+  end
+
+  def self.most_res
+    top_city = ""
+    top_res_count = ""
+    self.all.each do |city|
+      city_reservations = []
+      city.listings.each do |listing|
+        listing.reservations.each do |reservation|
+          city_reservations << reservation
+        end
+      end
+      if city_reservations.count > top_res_count.to_i 
+        top_res_count = city_reservations.count 
+        top_city = city 
+      end
+    end
+    top_city
+  end
+  
+
 end
