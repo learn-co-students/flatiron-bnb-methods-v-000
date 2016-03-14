7b2df0566763e956958e4d9e71eb6a74cafa0351
NStephenson <nickstephenson90@gmail.com>
2016-03-13 22:44:22 -0400

    made all tests pass

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,46 @@
+require 'pry'
+
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+
+  after_create :change_host_status
+  after_destroy :check_host_status
+
+  def change_host_status
+    host.host = true
+    host.save
+  end
+
+  def check_host_status
+    if host.listings.empty?
+      host.host = false
+      host.save
+    end
+  end
+
+  def average_review_rating
+    all_ratings = []
+    self.reviews.each do |review|
+      all_ratings << review.rating
+    end
+    all_ratings.sum/all_ratings.count.to_d
+  end
+
+  def available?(date)
+    reservations.none? do |res| 
+      if date > res.checkin
+        date < res.checkout
+      else
+        false
+      end
+    end
+  end
+
   
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,93 @@
+require 'pry'
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :invalid_same_ids 
+  validate :checkin_presence 
+  validate :checkout_presence 
+  validate :invalid_checkin 
+  validate :invalid_checkout 
+  validate :invalid_same_checkout_checkin 
+  validate :invalid_checkout_before_checkin 
+
+
+
+  def invalid_same_ids
+    # binding.pry
+    if listing.host == guest
+      errors.add(:guest, "Cannot make a reservation on you own listing.")
+    end
+  end
+
+  def checkin_presence
+    if checkin.nil?
+      errors.add(:checkin, "Must give a checkin time.")
+    end
+  end
+
+  def checkout_presence
+    if checkout.nil?
+      errors.add(:checkin, "Must give a checkin time.")
+    end
+  end
+
+  def invalid_checkin
+    if !checkin.nil?
+      if !listing.available?(checkin) 
+        errors.add(:checkin, "This listing is not available on that date")
+      end
+    else
+      errors.add(:checkin, "Must give a checkin time.")
+    end
+  end
+
+  def invalid_checkout
+    if !checkout.nil?
+      if !listing.available?(checkout)
+        errors.add(:checkout, "This listing is not available on that date")
+      end
+    else
+      errors.add(:checkout, "Must give a checkout time.")
+    end
+  end
+
+  def invalid_checkin_checkout
+    
+  end
+
+  def invalid_checkout_before_checkin
+    if !checkin.nil? && !checkout.nil?
+      if checkin > checkout
+        errors.add(:checkout, "Checkout can't be before checkin")
+      end
+    else
+      errors.add(:checkin, "Must give a checkin and checkout time.")
+    end
+  end
+
+  def invalid_same_checkout_checkin
+
+    if checkin == checkout
+      errors.add(:checkout, "Checkout can't be same as checkin")
+    end
+  end
+
+
+  def total_price
+    listing.price * duration
+  end
+
+
+  def duration
+    checkout - checkin
+  end
+
+
+
 end
+
+
+
