81c7186ed49345c47aa3e55597fc6500fe779fc0
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-21 21:04:59 +0000

    adds searchable concerns shared by City and Neighborhood models

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,9 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
   extend Concerns::Popular
+  include Concerns::Searchable 
+  
+  def neighborhood_openings(checkin_day, checkout_day)
+    find_availability(self, checkin_day, checkout_day)
+  end
 end
