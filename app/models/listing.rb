b7bd8152efcd02b6c08225114e66056d47d7546f
authorbeard <msnolan@gmail.com>
2016-03-20 23:09:34 -0400

    listing validation specs passing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,7 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
 
 
    def available(start, fin)
