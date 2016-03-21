978c51e5835ad683a406ff0c4ab6f161de1334f3
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-21 20:07:35 +0000

    adds listing model validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,10 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates :address, presence: true 
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood_id, presence: true
 end
