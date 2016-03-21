6acc152b9651f99cce123e39660706ffe6bf541f
authorbeard <msnolan@gmail.com>
2016-03-21 12:27:25 -0400

    added User#host_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,11 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
   has_many :guests, through: :listings
-  
+  has_many :host_reviews, through: :listings, source: :reviews
+
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :hosts, through: :trips, source: :listing, foreign_key: :host_id, class_name: "User"
 
   has_many :reviews, :foreign_key => 'guest_id'
-  
-  # def hosts
-  #   self.trips.select{|r| r.listing.host_id}
-
-  # end
-
 
 end
