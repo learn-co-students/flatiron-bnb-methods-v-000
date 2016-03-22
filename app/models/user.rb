85ad71030b330a7837327d3158c528e85ead8cc8
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-22 20:07:08 +0000

    adds instance methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,29 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
   
+  # Returns all of the guests a user has had as a host
+  def guests 
+    @guests = self.reservations.collect do |res|
+      res.guest
+    end
+  end
+  
+  # Returns all of the hosts a user has had as a guest
+  def hosts 
+    @hosts = self.trips.collect do |trip|
+      trip.listing.host
+    end
+  end
+  
+  # returns all of the reviews a host has received from guests
+  def host_reviews 
+    @host_reviews = []
+    self.reservations.each do |res| 
+      if res.review 
+        @host_reviews << res.review
+      end
+    end
+    @host_reviews
+  end
+  
 end
