0e3dee876db46ac88574a944a171dd07e158910b
Tom Johns <tommyskies@hotmail.com>
2016-03-22 20:37:32 -0600

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,20 +4,41 @@ class Reservation < ActiveRecord::Base
   has_one :review
   
   validates :checkin, :checkout, presence: true
-  validate :not_same_as_host
-  validate :listing_available
+  validate :not_same_as_host, :listing_available, :checkin_is_first
   
-  def not_same_as_host
-  	if guest_id == listing.host_id
-  		errors.add(:guest_id, "can't be the same as host")
-  	end
+  
+  def duration
+    (self.checkout - self.checkin).to_i
   end
+  
+  def total_price
+    listing.price * self.duration
+  end
+  
+  
+  private
+  
+    def not_same_as_host
+    	if guest_id == listing.host_id
+    		errors.add(:guest_id, "can't be the same as host")
+    	end
+    end
 
-  def listing_available
-  	if !listing.open_during?(checkin..checkout)
-  		errors.add(:checkin, "not available")
-  		errors.add(:checkout, "not available")
-  	end
-  end
+    def listing_available
+    	listing.reservations.each do |r|
+        range = r.checkin..r.checkout
+        if range === self.checkin || range === self.checkout
+          errors.add(:guest_id, "date is unavailable")
+        end
+      end
+    end
+    
+    def checkin_is_first
+      if checkin && checkout
+        if checkout <= checkin
+          errors.add(:guest_id, "checkout date must be after checkin")
+        end
+      end
+    end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,14 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
   
-  validates :rating, :description, :reservation_id, presence: true
+  validates_presence_of :rating, :description, :reservation_id
+  validate :res_accepted
+  
+  private
+  	def res_accepted
+  		errors.add(:reservation_id, "invalid reservation") unless Reservation.exists?(reservation_id) && Reservation.find(reservation_id).status == "accepted" && Reservation.find(reservation_id).checkout < Date.today
+  	end
+  		
+
 
 end
