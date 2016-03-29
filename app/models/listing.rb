41a45be29f5f131e3d6e24c0a4e140b632e2e499
esimonian <eli.simonian@gmail.com>
2016-03-28 17:09:01 -0700

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,28 @@
 class Listing < ActiveRecord::Base
+  validates_presence_of :listing_type, :address, :title, :description, :price, :neighborhood_id
+
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  before_save :make_host
+  before_destroy :host_status
+
+
+  def average_review_rating
+    self.reviews.average(:rating)
+  end
+
+  private
+
+  def make_host
+    h = self.host
+    h.update(host:true)
+  end
+
+  def host_status
+  self.host.update(:host => false) if self.host.listings.count <= 1
+  end
+
 end
