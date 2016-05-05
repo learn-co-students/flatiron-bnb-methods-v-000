0a8dec4e3b909015fadccd957f96fd1fa9bd6b16
mhnguyen289 <mhnguyen289@gmail.com>
2016-05-05 01:43:29 -0700

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :description, :rating, :reservation, presence: true
+  validate :valid_review
+
+  private
+
+  def valid_review
+    errors.add(:reservation_id, "doesn't exist") unless Reservation.exists?(reservation_id) && Reservation.find(reservation_id).status == "accepted" && Reservation.find(reservation_id).checkout < Date.today
+  end
+
 end
