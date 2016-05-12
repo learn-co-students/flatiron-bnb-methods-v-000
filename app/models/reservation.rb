dcbbbe50697e886ccd67f96d263c38fbe0d91373
ShiLi007 <lishidaniel@gmail.com>
2016-05-12 00:36:21 -0400

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,42 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validate :is_host
+ 
+
+  validate :trip_dates
+ 
+###VALIDATION METHODS 
+  def is_host
+    errors.add(:guest_id, "guest cannot be host") unless self.guest_id != self.listing.host_id
+  end
+
+  def trip_dates
+
+    if self.checkin.nil? || self.checkout.nil?
+      errors.add(:checkin, "Must have checkin and checkout dates")
+    elsif !is_available
+      errors.add(:checkin, "not available.")
+    elsif self.checkin >= self.checkout
+      errors.add(:checkin, "must be at least one day before checkout.")
+    end
+  end
+
+  def is_available
+    listing.available(self.checkin, self.checkout)
+  end
+
+###INSTANCE METHODS
+
+  def duration
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    self.listing.price.to_f * self.duration
+  end
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
+  has_many :guests, through: :listings
+  has_many :host_reviews, through: :listings, source: :reviews
+
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
+  has_many :hosts, through: :trips, source: :listing, foreign_key: :host_id, class_name: "User"
+
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
 end
