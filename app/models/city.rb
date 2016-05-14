5c8dd5bfb807cfe206c13e5df2ff717185eb026b
morgvanny <morgvanny@gmail.com>
2016-05-14 16:17:31 -0600

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,15 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  include LocationQuerys
+  extend LocationQuerys::ClassMethods
 
+  def city_openings(begin_date,end_date)
+    self.listings.collect do |listing|
+      if !listing.listing_reserved?(begin_date, end_date)
+          listing
+      end
+    end
+  end
+
+end
