ee33bf49e19cb99b08d269e274e2e1d8ae0b8bae
evanscloud <dijkstrascloud@gmail.com>
2016-05-16 11:06:41 -0400

    Build methods, validations, and associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,20 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
+  before_create :make_host_status
+  after_destroy :remove_host_status
+
+  def make_host_status
+    self.host.update(host: true)
+  end
+
+  def remove_host_status
+    self.host.update(host: false) if self.host.listings.empty?
+  end
+
+  def average_review_rating
+    avg_rating = self.reviews.map { |review| review.rating }.sum
+    avg_rating.to_f / self.reviews.count.to_f
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates_presence_of :rating, :description, :reservation
+  validate :valid_review
 
+  def valid_review
+    unless self.reservation && self.reservation.status == "accepted" && self.reservation.checkout <= Date.today
+      errors.add(:valid_review, "Please check out before writing a review.")
+    end
+  end
 end
