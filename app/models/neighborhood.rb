0037109ef67da33bf2a11de16db250a00c002e80
Ziv Zamechek <ziv921@yahoo.com>
2016-05-18 13:42:55 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,11 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  
+  include Reservable
+
+  def neighborhood_openings(startDate, endDate)
+    openings(startDate, endDate)
+  end
 
 end
