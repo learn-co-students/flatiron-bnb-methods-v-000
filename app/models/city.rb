9df21abbdc743bc386d1666e1feae3732a9bc879
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-22 02:38:22 +0100

    solve #city_openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,19 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    sql = <<-SQL
+      listings.id NOT IN (
+        SELECT distinct(listings.id) FROM listings
+        JOIN reservations ON listings.id = reservations.listing_id
+        WHERE reservations.checkin BETWEEN ? AND ?
+        OR reservations.checkout BETWEEN ? AND ?
+      )
+    SQL
+
+    listings.where(sql, start_date, end_date, start_date, end_date)
+  end
+
+
 end
 
