c80ccd7e5f8b5f7df9ce54591b63346fc74832e8
gnfisher <me@gnfisher.com>
2016-05-23 16:14:02 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -8,25 +8,4 @@ class Neighborhood < ActiveRecord::Base
     listings.available_listings_for_dates(start_date, end_date)
   end   
 
-#  def res_to_listings_ratio
-#    total_reservations.to_f / listings.count.to_f
-#  end
-#
-#  def total_reservations
-#    listings.reduce(0) { |total, listing| listing.reservations.count + total }
-#  end
-#
-#  class << self
-#    def highest_ratio_res_to_listings
-#      has_listings.max { |a,b| a.res_to_listings_ratio <=> b.res_to_listings_ratio }
-#    end
-#
-#    def most_res
-#      has_listings.max { |a,b| a.total_reservations <=> b.total_reservations }
-#    end
-#
-#    def has_listings
-#      all.select { |city| city.listings.any? }
-#    end
-#  end
 end
