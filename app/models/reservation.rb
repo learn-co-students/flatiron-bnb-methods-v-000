f2bda9f2fb1a0ae02a2f57069a6f3bd37f3595ec
LKWLaLa <lkwlala@gmail.com>
2016-05-26 23:14:24 -0400

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,8 @@ class Reservation < ActiveRecord::Base
   validates_presence_of :checkin, :checkout
   validate :different_dates?, if: "checkin.present? && checkout.present?"
   validate :checkout_before_checkin?, if: "checkin.present? && checkout.present?"
+  validate :host_different_from_guest?
+  validate :listing_available?, if: "checkin.present? && checkout.present?"
 
   def duration
     (self.checkin...self.checkout).to_a.length
@@ -24,6 +26,21 @@ class Reservation < ActiveRecord::Base
    def checkout_before_checkin?
     if checkin > checkout
       errors.add(:reservation, "Checkin date cannot be greater than checkout date.")
+    end
+  end
+
+  def host_different_from_guest?
+    if self.guest_id == self.listing.host_id
+      errors.add(:reservation, "You cannot make a reservation on your own listing.")
+    end
+  end
+
+  def listing_available?
+    self.listing.reservations.each do |r|
+      dates = (r.checkin..r.checkout).to_a
+      if (dates.include?(self.checkin) || dates.include?(self.checkout))
+        errors.add(:reservation, "These dates are not available.")
+      end
     end
   end
 
