e2b6d1de5340a8d5f0a1621732e835eac719b474
Brian <useyourbrian@gmail.com>
2016-05-28 09:33:14 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,47 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(beginning_date, end_date)
+    available_listings = []
+    listings.each do |listing|
+      if listing.reservations.where("checkin >= ? AND checkout <= ?", beginning_date, end_date).empty?
+        available_listings << listing
+      end
+    end
+    available_listings
+  end
+
+
+  def self.highest_ratio_res_to_listings
+
+    neighborhoods_with_scores = {}
+
+    self.all.each do |nabe|
+      number_of_listings = nabe.listings.count
+      reservation_counts = nabe.listings.map { |listing| listing.reservations.count }
+      sum_of_reservations = reservation_counts.inject(0) { |sum, count| sum + count }
+      if sum_of_reservations == 0
+        ratio_score = 2 # edge case
+      else
+        ratio_score = number_of_listings.to_f / sum_of_reservations.to_f
+      end
+
+      neighborhoods_with_scores[nabe] = ratio_score
+    end
+
+    neighborhoods_with_scores.min_by { |nabe, score| score }.first
+  end
+
+  def self.most_res
+    neighborhoods_with_reservation_counts = {}
+
+    self.all.each do |nabe|
+      reservation_counts = nabe.listings.map { |listing| listing.reservations.count }
+      sum_of_reservations = reservation_counts.inject(0) { |sum, count| sum + count }
+      neighborhoods_with_reservation_counts[nabe] = sum_of_reservations
+    end
+
+    neighborhoods_with_reservation_counts.max_by { |nabe, reservations| reservations }.first
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,57 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
-end
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
+  validate :host_is_not_guest, :available_checkin, :available_checkout, :sensible_times
+
+  def duration
+    duration = checkout.to_date - checkin.to_date
+    duration.to_i
+  end
+
+  def total_price
+    total_price = listing.price * duration
+    total_price.to_f
+  end
+
+  private
+    def host_is_not_guest
+      if guest == listing.host
+        errors.add(:guest_id, "you cannot make a reservation on your own listing")
+      end
+    end
+
+    def available_checkin
+      if id.nil?
+        id_for_lookup = 0
+      else
+        id_for_lookup = id
+      end
+      bad_checkin_times = listing.reservations.where("checkin <= ? AND checkout >= ? AND id != ?", checkin, checkin, id_for_lookup)
+      if !bad_checkin_times.empty?
+        errors.add(:checkin, "listing must be available at checkin")
+      end
+    end
+
+    def available_checkout
+      if id.nil?
+        id_for_lookup = 0
+      else
+        id_for_lookup = id
+      end
+      bad_checkout_times = listing.reservations.where("checkout >= ? AND checkin <= ? AND id != ?", checkout, checkout, id_for_lookup)
+      if !bad_checkout_times.empty?
+        errors.add(:checkout, "listing must be available at checkout")
+      end
+    end
+
+    def sensible_times
+      return if errors[:checkin].present? || errors[:checkout].present? # cannot rely on builtin validations to be performed in order
+      if checkin >= checkout
+        errors.add(:checkin, "checkin time must be before checkout time")
+      end
+    end
+    
+  end
