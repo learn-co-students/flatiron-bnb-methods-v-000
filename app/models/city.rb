c0aacaab29a654c685a2aebee81a5c7941d4be9d
PeteAJ <peter.jacobson@gmail.com>
2016-06-07 13:58:30 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,41 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  
 
+  def city_openings(start_date, end_date)
+    Listing.all.each do |listing|
+      listing.reservations.each do |res|
+        start_date <= res.checkout.to_s && end_date >= res.checkin.to_s
+      end
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+      ratios = {}
+      self.all.each do |city|
+        counter = 0
+        city.listings.each do |a|
+          counter += a.reservations.count
+        end
+        ratios[city] = (counter)/(city.listings.count)
+      end
+      ratios.key(ratios.values.sort.last)
+  end
+
+  def self.most_res
+    res = {}
+    self.all.each do |city|
+      counter = 0
+      city.listings.each do |a|
+        counter += a.reservations.count
+      end
+      res[city] = counter
+    end
+    res.key(res.values.sort.last)
+  end
+
+
+
+
+end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,37 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+ validate :not_same_host_and_guest, :available, :checkin_before_checkout
+
+  def not_same_host_and_guest
+    if guest_id == listing.host_id
+      errors.add(:guest_id, "You cannot reserve your own apartment.")
+    end
+  end
+
+  def available
+    listing.reservations.where.not(id: id).each do |reservation|
+      reservation_date_range = reservation.checkin..reservation.checkout
+      if reservation_date_range === checkin || reservation_date_range === checkout
+        errors.add(:guest_id, "Not available during the requested dates.")
+      end
+    end
+  end
+
+  def checkin_before_checkout
+    if checkin && checkout && checkin >= checkout
+      errors.add(:guest_id, "Your check-out date needs to be after your check-in.")
+    end
+  end
+
+  def duration
+    checkout - checkin
+  end
+
+  def total_price
+    listing.price * duration
+  end
+  
 end
