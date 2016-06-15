31b4d01c56775bf8afaebcad4fe184fcda641b7d
lcorr8 <l.corr.v@gmail.com>
2016-06-15 17:34:37 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,18 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
+  #has_many :hosts, through: :reservations, :class_name => "User" 
+
   
+  def guests
+    self.reservations.collect{|reservation| reservation.guest}
+  end
+
+  def hosts
+  end
+
+  def host_reviews
+    #self.guest
+  end
+
 end
