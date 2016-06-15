ee3c88a91be568b4f70fe150cf2250183913b826
lcorr8 <l.corr.v@gmail.com>
2016-06-15 14:32:12 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,20 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(checkin, checkout)
+    #uses checkin and checkout to determine
+    #which listings are not booked/available listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    #knows the city with the highest ratio of reservations to listings
+  end
+
+  def self.most_res
+    #knows the city with the most reservations
+  end
+
+
+
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,7 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  
+
 end
