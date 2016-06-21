3411204e8a02c13cb8e2afec844579d1d0b7711b
depippo <jdepippo@gmail.com>
2016-06-21 18:24:05 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,19 @@
 class Neighborhood < ActiveRecord::Base
+  include Ratios
   belongs_to :city
   has_many :listings
+  has_many :reservations, :through => :listings
+
+  def neighborhood_openings(date_1, date_2)
+    start_date = Date.parse(date_1)
+    end_date = Date.parse(date_2)
+    available_listings = []
+    listings.each do |listing|
+      unless listing.reservations.any? { |res| start_date.between?(res.checkin, res.checkout) || end_date.between?(res.checkin, res.checkout) }
+        available_listings << listing
+      end
+    end
+    available_listings
+  end 
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,40 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, :checkout, presence: true
+  validate :guest_is_not_host, :available, :checkout_time
+
+  def duration
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    self.listing.price * duration
+  end
+
+  private
+
+  def guest_is_not_host
+    if self.guest_id == self.listing.host_id
+      errors.add(:guest_id, "Guest and host cannot be the same.")
+    end
+  end
+
+  def available
+    self.listing.reservations.each do |res|
+      booked_dates = res.checkin..res.checkout
+      if booked_dates === self.checkin || booked_dates === self.checkout
+        errors.add(:guest_id, "Those dates have already been booked.")
+      end
+    end
+  end
+
+  def checkout_time
+    if self.checkin && self.checkout
+      if self.checkout <= self.checkin
+        errors.add(:guest_id, "Checkout time must be after checkin time.")
+      end
+    end
+  end
 
 end
