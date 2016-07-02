d39e0c515bc2310e4e85e0dbbaf6d5f1c6f4f600
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-07-02 21:18:33 +0000

    wrote most of the listing methods

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,19 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+  
+  before_save :make_host
+  before_destroy :undo_host
+  
+  
+  private
+  
+  def make_host
+    self.host.update(host: true) if self.host
+  end
+  
+  def undo_host
+    self.host.update(host: false) if self.host.listings.count == 1
+  end  
 end
