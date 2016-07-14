dd42961cd7dd013f4b399bd3d5dcabe305bd6abf
mcasciato <michael.casciato@icloud.com>
2016-07-14 12:49:10 -0400

    reservation model tests passing; what a relief

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,48 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :host_is_not_guest, :checkin_before_checkout, :checkin_not_checkout
+  before_validation :available?
+
+  def total_price
+    listing.price * duration
+  end
+
+  def duration
+    checkout - checkin
+  end
+
+  private
+
+  def host_is_not_guest
+    if self.listing.host_id == self.guest_id
+      errors.add(:host_is_not_guest, "You cannot make a reservation on your own listing")
+    end
+  end
+
+  def checkin_before_checkout
+    if checkout && checkin && checkout < checkin
+      errors.add(:reservation, "Please try dates again.")
+    end
+  end
+
+  def checkin_not_checkout
+    if checkin == checkout
+      errors.add(:reservation, "Please try dates again.")
+    end
+  end
+
+  def available?
+    if checkout && checkin
+      listing.reservations.each do |r|
+        if r.checkin <= checkout && r.checkout >= checkin
+          errors.add(:reservation, "Please try dates again.")
+        end
+      end
+    end
+  end
+
+
 end
