9c5c5af03f1aa5d6d029324b85fe74b516d3eaf5
mcasciato <michael.casciato@icloud.com>
2016-07-14 14:48:49 -0400

    review model tests passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates_presence_of :description, :rating, :reservation
+  validate :checkout?
 
+  def checkout?
+    if r = Reservation.find_by(id: reservation_id)
+      if Date.today < r.checkout
+        errors.add(:checkout, "You must be checked out to write a review!")
+      end
+    end
+  end
 end
