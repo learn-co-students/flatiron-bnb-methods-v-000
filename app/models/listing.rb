95fbc9d0317e1130c4709df654dcb799e3f24150
phaltoe <reverse9@live.com>
2016-07-18 14:20:06 -0300

    few specs passing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,38 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+
+  before_save :change_user_to_host
+  before_destroy :change_host_to_user
+
+  def average_review_rating
+    @total_ratings = 0.0
+    @number_of_reviews = 0
+
+    reviews.each do |review|
+      @total_ratings += review.rating
+      @number_of_reviews += 1 
+    end
+    (@total_ratings / @number_of_reviews)
+  end
+
+
+  private
+
+  def change_user_to_host
+    user = self.host
+    user.update(host: true)
+    user.save
+  end
+
+  def change_host_to_user
+    user = self.host
+    if user.listings.count == 1
+      user.update(host: false)
+      user.save
+    end
+  end
   
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,13 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates_presence_of :checkin, :checkout
+
+  def self.available?(time)
+    @date = Time.parse(time)
+    @reservations = self.all
+    @reservations.collect do |reservation|
+      @date >= reservation.checkout && @date <= reservation.checkin
+    end
+  end
 end
