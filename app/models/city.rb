34f544f4515713b41ccd9423958684c23b4cf484
Ann Lee <aplee29@users.noreply.github.com>
2016-07-19 10:29:28 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,11 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
+  include Reservable
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
 end
-
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,41 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates_presence_of :checkin, :checkout
+  validate :guest_not_host, :checkout_after_checkin, :available
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
+
+  private
+
+  def guest_not_host # Confirms that guest and host aren't the same user
+    if listing.host.id == guest_id
+      errors.add(:guest_id, "You can't book your own listing.")
+    end
+  end
+
+  def checkout_after_checkin # Confirms that checkout is after checkin
+    if checkin && checkout && checkout <= checkin
+      errors.add(:guest_id, "Checkout date must be after checkin date.")
+    end
+  end
+
+  def available # Confirms no conflicts among reservations
+    unless self.checkin.nil? || self.checkout.nil?
+      listing.reservations.each do |reservation|
+        # Returns a range of dates from checkin ~ checkout for each listing
+        reserved = reservation.checkin..reservation.checkout
+        # If user-selected checkin/checkout dates are within range of a reserved booking
+        if reserved === self.checkin || reserved === self.checkout
+          errors.add(:guest_id, "The dates you've selected are already booked.")
+        end
+      end
+    end
+  end
 end
