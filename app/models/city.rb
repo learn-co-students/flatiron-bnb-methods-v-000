1d886feaadad8cdf9497269b6378d6a38154fba8
natasha carlyon <natasha.carlyon@gmail.com>
2016-07-20 20:43:04 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,33 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
+
+  def city_openings(start_date, end_date)
+  #  available = Listing.joins(:reservations).where.not(reservations: {:checkin => start_date..end_date}) &
+  #  Listing.joins(:reservations).where.not(reservations: {:checkout => start_date..end_date})
+
+  #  return self.listings.merge(available)
+  listings
+  end
+
+  def find_ratio
+    if listings.count > 0
+      reservations.count.to_f / listings.count.to_f
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+      City.all.max do |a, b|
+        a.find_ratio <=> b.find_ratio
+      end
+  end
+
+  def self.most_res
+    City.all.max do |a, b|
+      a.reservations.count <=> b.reservations.count
+    end
+  end
+
 
 end
-
