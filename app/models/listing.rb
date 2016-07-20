92c3d67b66face85ca6fd8b5d24337a20d5b366d
danielriley06 <driley3@gmail.com>
2016-07-20 01:01:32 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,36 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+
+  after_create :set_host_true
+  after_destroy :set_host_false
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  private
+
+  def self.available(start_date, end_date)
+    if start_date && end_date
+      joins(:reservations).
+        where.not(reservations: {checkin: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {checkout: start_date..end_date})
+    else
+      []
+    end
+
+  end
+
+  def set_host_true
+    host.update(host: true)
+  end
+
+  def set_host_false
+    host.update(host: false) if host.listings.none?
+  end
+
 end
