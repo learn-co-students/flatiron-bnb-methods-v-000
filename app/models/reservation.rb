cce7449a45c5ae2f7ca7d48621cffd6dcb4b9841
phaltoe <reverse9@live.com>
2016-07-21 13:42:58 -0300

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,12 +4,42 @@ class Reservation < ActiveRecord::Base
   has_one :review
 
   validates_presence_of :checkin, :checkout
+  validate :checkin_before_checkout, :host_is_not_guest, :checkin_not_checkout
+  before_validation :available?
 
-  def self.available?(time)
-    @date = Time.parse(time)
-    @reservations = self.all
-    @reservations.collect do |reservation|
-      @date >= reservation.checkout && @date <= reservation.checkin
+  def available?
+    if checkin && checkout
+      listing.reservations.each do |reservation|
+        if reservation.checkin <= checkout && reservation.checkout >= checkin
+          errors.add(:reservation, "Please try date again")
+        end
+      end
     end
+  end
+
+  def checkin_before_checkout
+    if checkin && checkout && checkout < checkin
+      errors.add(:reservation, "Please try date again.")
+    end
+  end
+
+  def host_is_not_guest
+    if self.listing.host_id == self.guest_id
+     errors.add(:host_is_not_guest, "You cannot make a Reservation on your own listing")
+    end
+  end
+
+  def checkin_not_checkout
+    if checkin == checkout
+     errors.add(:reservation, "Please try date again.")
+    end
+  end
+
+  def duration
+    checkout - checkin
+  end
+
+  def total_price
+    listing.price * duration
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,15 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
-
-  validates_presence_of :rating, :description
+  validates_presence_of :description, :rating, :reservation
+  validate :checkout?
+ 
+  def checkout?
+    if reservation = Reservation.find_by(id: reservation_id)
+      if Date.today < reservation.checkout
+        errors.add(:checkout, "Plese write a review after checking out.")
+      end
+    end
+  end
 
 end
