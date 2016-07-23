59185576c369cbb02647c6e60346f66819b2c3f9
ndaniele <n.daniele@gmail.com>
2016-07-23 15:02:43 -0400

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,35 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+
+  def neighborhood_openings(startdate, enddate)
+    availables = []
+    self.listings.each do |listing| 
+      if listing.reservations.empty?
+        availables << listing
+      else
+        listing.reservations.each do |reservation|
+          if reservation.checkin >= enddate.to_datetime || reservation.checkout <= startdate.to_datetime
+            availables << reservation.listing
+          end
+        end
+      end
+    end
+    availables
+   end
+
+   def self.highest_ratio_res_to_listings
+      self.all.max_by do |place| 
+        if place.listings.count == 0
+          0
+        else
+          place.listings.map{|l| l.reservations.count}.sum / place.listings.count
+        end
+      end
+    end
+
+  def self.most_res
+    self.all.max_by {|place| place.listings.map {|listing| listing.reservations.count}}
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,39 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :same_date
+  validate :same_ids
+  validate :checkin_before_checkout
+  validate :unavailable_dates
+
+
+
+  def same_date
+    errors.add(:checkin, 'checkin and checkout cannot be on the same day.') if self.checkin == self.checkout
+  end
+
+  def same_ids
+    errors.add(:listing, 'cannont book your own listing.') if self.guest_id == self.listing.host_id
+  end
+
+  def checkin_before_checkout
+    errors.add(:Reservation, 'checkin date cannot be later than checkout date.') if self.checkin && self.checkout && self.checkin > self.checkout
+  end
+
+  def unavailable_dates
+    errors.add(:reservation, 'unavailable dates.') if self.checkin && self.checkout && self.listing.reservations.any?{|res|(res.checkin - self.checkout) * (self.checkin - res.checkout) >= 0}
+  end
+
+  def duration
+    self.checkout - self.checkin
+  end
+
+  def total_price
+    self.listing.price * self.duration
+  end
+
+
+
 end
