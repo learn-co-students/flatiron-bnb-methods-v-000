184e66fe508747bfd1f8ce522cfc3eaedb2aae35
Christian Whitesides <whitesidescr@gmail.com>
2016-07-23 10:31:05 -0600

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,8 +5,22 @@ class Reservation < ActiveRecord::Base
 
   validates_presence_of :checkin
   validates_presence_of :checkout
-  validate :no_res_for_host
+  validate :no_res_for_host, :available, :min_one_day_stay
   
+
+  # Instance methods
+  def duration
+    if checkin && checkout
+      (self.checkout - self.checkin).to_i
+    end
+  end
+
+  def total_price
+    if duration
+      self.listing.price*duration
+    end
+  end
+
 
   private
 
@@ -16,6 +30,25 @@ class Reservation < ActiveRecord::Base
     end
   end
 
-  
+  def available
+    reservation = Reservation.where(listing_id: listing_id)
+    if checkin && checkout
+      reservation.each do |res|
+        if id == res.id
+          break
+        elsif checkin.between?(res.checkin, res.checkout)
+          errors.add(:checkin, "Listing is unavailable")
+        elsif checkout.between?(res.checkin, res.checkout)
+          errors.add(:checkin, "Listing is unavailable")
+        end
+      end
+    end
+  end
 
+  def min_one_day_stay
+    if duration && duration <= 0
+      errors.add(:checkout, "This reservation is only for one day.")
+    end
+  end
+  
 end
