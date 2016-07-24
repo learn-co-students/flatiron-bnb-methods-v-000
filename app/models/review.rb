b0e37f4963ad57c0f8e25cb2f0c9d3284314bc20
jbok4 <jbok4@aol.com>
2016-07-24 12:05:22 -0400

    reviews complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :reservation, presence: true
+  validate :checked_out
+
+  def checked_out
+    if reserved = Reservation.find_by(id: reservation_id)
+       if Date.today < reserved.checkout
+        errors.add(:checkout, "You can't send a review until you have checked out.")
+      end
+    end
+  end
+
 end
