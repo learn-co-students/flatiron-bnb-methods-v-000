4efb699a78b55f3fcea4855e37de3ee444fa2dcc
DEREJE GEMECHU <dereje88@gmail.com>
2016-07-26 02:01:43 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,56 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
 
+  def city_openings(start_date, end_date)
+    list = []
+    self.listings.each do |l|
+      if l.reservations.empty?
+        list << l
+      else
+        l.reservations.each do |r|
+          if ((r.checkin...r.checkout) === start_date.to_date || (r.checkin...r.checkout) === end_date.to_date)
+            break
+          else
+            list << l
+          end
+        end
+      end
+    end
+    list
+    #binding.pry
+  end
+
+  def self.highest_ratio_res_to_listings
+    highest_resev_per_list = ""
+    no_of_list = 0
+    self.all.each do |city|
+      city.listings.each do |list|
+        if no_of_list < list.reservations.count
+          no_of_list = list.reservations.count
+          highest_resev_per_list = city
+        end
+      end
+    end
+    highest_resev_per_list
+  end
+
+
+  def self.most_res
+    highest_resev_per_list = ""
+    no_of_res = 0
+    self.all.each do |city|
+      tmp = 0
+      city.listings.each do |list|
+          tmp += list.reservations.count
+      end
+      if tmp >= no_of_res
+        no_of_res = tmp
+        highest_resev_per_list = city
+      end
+    end
+    highest_resev_per_list
+
+  end
+
+end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,43 @@
 class Listing < ActiveRecord::Base
-  belongs_to :neighborhood
+  belongs_to :neighborhood, required: true
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :listing_type, presence: true
+  validates :price, presence: true
+  validates :title, presence: true
+
+  after_save :set_host, on: :create
+
+  after_destroy :set_host
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  private
+
+  def set_host
+    if self.host.listings.empty?
+      self.host.update(host: false)
+    else
+      self.host.update(host: true)
+    end
+  end
+
+  def self.available(start_date, end_date)
+    if (start_date <= end_date && end_date >= start_date)
+      joins(:reservations).
+        where.not(reservations: {checkin: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {checkout: start_date..end_date})
+    else
+      []
+    end
+  end
+
 end
