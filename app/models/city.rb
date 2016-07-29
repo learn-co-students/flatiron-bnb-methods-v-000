0fc70b5f5a317b4147a2881282fac29522d3f7d4
Saul Ocampo <digitalh2o@gmail.com>
2016-07-28 21:47:18 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,17 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
+
+  include Reservable
+
+  def city_openings(startdate, enddate)
+  	Listing.all.each do |listing|
+  		listing.reservations.each do |reserve|
+  			startdate <= reserve.checkout.to_s && enddate >= reserve.checkin.to_s
+  		end
+  	end  	
+  end
 
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,16 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates :rating, presence: true
+
+  validate :can_leave_review
+
+  private
+
+  def can_leave_review
+  	if !reservation || reservation.checkout > Date.today 
+  		errors.add(:review, "You are unable to leave a review without a reservation")
+  	end
+  end
 
 end
