456240f4e9c10ae27b4f2e324a613f6e7854355d
kaileeagray <kaileeagray@gmail.com>
2016-08-04 14:57:25 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,18 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(checkin, checkout)
+    neighborhood_ids = Neighborhood.where(city_id: self.id).pluck(:id) #return all neighborhood id in city
+    # collect all listings in the neighborhood
+    Listing.where(neighborhood_id: neighborhood_ids).collect do |listing|
+      if listing.reservations.any? do |reservation|
+          (reservation.checkin...reservation.checkout).overlaps?(checkin...checkout)
+        end
+        nil
+      else
+        listing
+      end
+    end
+  end
 
+end
