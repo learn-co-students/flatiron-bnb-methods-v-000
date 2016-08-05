b99fea796f89a265c5d81b312b749a0e15b22846
cmiller36 <m.calliemiller@gmail.com>
2016-08-05 15:19:47 -0400

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,36 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates_presence_of :checkin, :checkout
+  validate :checkin_is_before_checkout
+  validate :host_cannot_be_guest
+  validate :dates_available
+
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
+
+  def checkin_is_before_checkout
+    if checkout && checkin && checkout <= checkin
+      errors.add(:guest_id, "Your checkout date must be after your checkin date.")
+    end
+  end
+
+  def host_cannot_be_guest
+    if listing.host_id == guest_id
+      errors.add(:guest_id, "You cannot book your own listing.")
+    end
+  end
+
+  def dates_available
+    if self.checkin && self.checkout && self.listing.reservations.any?{|r|(r.checkin - self.checkout) * (self.checkin - r.checkout) >= 0}
+      errors.add(:guest_id, "Sorry, your dates are not available.")
+    end
+  end
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates_presence_of :rating, :description, :reservation
+  validate :valid_review
+
+  def valid_review
+    if !reservation || reservation.checkout > Date.today || reservation.status != "accepted"
+    errors.add(:reservation, "You are unable to leave a review at this time.")
+    end 
+  end
+
 end
