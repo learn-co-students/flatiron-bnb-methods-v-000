9a2d742afcf48fd1fd50af2fdd7fa7f0671737e7
Joe <jsbenning4@gmail.com>
2016-08-06 19:34:55 -0400

    Added official approach for comparison

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -34,3 +34,57 @@ class Listing < ActiveRecord::Base
     avg = (ratings.inject(0){|sum, x| sum + x} / ratings.length.to_f)
   end 
 end
+
+# Official solution with notes
+# class Listing < ActiveRecord::Base
+#   belongs_to :neighborhood, required: true
+#   belongs_to :host, :class_name => "User"
+
+#   has_many :reservations
+#   has_many :reviews, :through => :reservations
+#   has_many :guests, :class_name => "User", :through => :reservations
+
+#   validates :address, presence: true
+#   validates :description, presence: true
+#   validates :listing_type, presence: true
+#   validates :price, presence: true
+#   validates :title, presence: true
+
+#   after_save :set_host_as_host
+#   before_destroy :unset_host_as_host
+
+#   def average_review_rating
+#     reviews.average(:rating)
+#   end
+
+#   private
+
+#   def self.available(start_date, end_date)
+#     if start_date && end_date
+#       joins(:reservations).
+#         where.not(reservations: {check_in: start_date..end_date}) &
+#       joins(:reservations).
+#         where.not(reservations: {check_out: start_date..end_date})
+#     else
+#       []
+#     end
+#   end
+
+
+#   # it feels to me like part of what makes this complicated is
+#   # that we have column in the database called is_host, 
+#   # but instead this could just be a method, and then rely on that..
+#   # not sure if it's worth the effort though.
+#   def unset_host_as_host
+#     # remove .id
+#     if Listing.where(host: host).where.not(id: id).empty?
+#       host.update(is_host: false)
+#     end
+#   end
+
+#   def set_host_as_host
+#     unless host.is_host?
+#       host.update(is_host: true)
+#     end
+#   end
+# end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,7 @@ class Reservation < ActiveRecord::Base
   validates :checkin, presence: true
   validates :checkout, presence: true
   validate :valid_dates?
-  #after_create :accept
+
 
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
@@ -16,9 +16,7 @@ class Reservation < ActiveRecord::Base
     total_price = (self.duration.to_i * self.listing.price.to_i)
   end
 
-  # def accept
-  #   self.status = "accepted"
-  # end
+
 
   def valid_dates?
     if self.checkin && self.checkout
@@ -52,3 +50,47 @@ class Reservation < ActiveRecord::Base
   end
 
 end
+
+#Official solution:
+
+# class Reservation < ActiveRecord::Base
+#   belongs_to :listing
+#   belongs_to :guest, :class_name => "User"
+
+#   has_one :review
+
+#   validates :check_in, :check_out, presence: true
+#   validate :available, :check_out_after_check_in, :guest_and_host_not_the_same
+
+#   def duration
+#     (check_out - check_in).to_i
+#   end
+
+#   def total_price
+#     listing.price * duration
+#   end
+
+#   private
+  
+
+#   def available
+#     Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+#       booked_dates = r.check_in..r.check_out
+#       if booked_dates === check_in || booked_dates === check_out
+#         errors.add(:guest_id, "Sorry, this place isn't available during your requested dates.")
+#       end
+#     end
+#   end
+
+#   def guest_and_host_not_the_same
+#     if guest_id == listing.host_id
+#       errors.add(:guest_id, "You can't book your own apartment.")
+#     end
+#   end
+
+#   def check_out_after_check_in
+#     if check_out && check_in && check_out <= check_in
+#       errors.add(:guest_id, "Your check-out date needs to be after your check-in.")
+#     end
+#   end
+# end
