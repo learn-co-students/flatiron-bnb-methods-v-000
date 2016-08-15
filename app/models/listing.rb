b5e24d38f17fb30d13d4afd66b7673e471dc5480
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-15 16:57:16 -0400

    after_create question

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,38 @@
 class Listing < ActiveRecord::Base
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
+  after_create :true_host
+
+  def true_host
+    self.host.host = true
+  end
+
+  def available?(checkin, checkout)
+    if self.reservations.any? {|x| checkin.to_date.between?(x.checkin, x.checkout) || checkout.to_date.between?(x.checkin, x.checkout)}
+      false
+    else
+      true
+    end
+  end
+
+  def availabilities(checkin, checkout)
+    array = []
+    self.reservations.each do |x|
+      if checkin.to_date.between?(x.checkin, x.checkout) || checkout.to_date.between?(x.checkin, x.checkout)
+      else
+        array << x
+      end
+    end
+    array
+  end
   
 end
