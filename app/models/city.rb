0b2b9aecd0cfb1ef92b1b711a08eaac488ef9bf7
kirangp <kirangp@gmail.com>
2016-08-17 22:15:40 +1000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,31 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, through: :listings
+
+
+
+  def city_openings(d1, d2)
+    listings.each do |l|
+      l.reservations.each do |r|
+        d1.to_date > r.checkout && d2.to_date < r.checkin
+      end
+    end
+  end
+
+
+   def self.highest_ratio_res_to_listings
+    ratio = Hash.new
+    City.all.each do |c|
+     ratio[c] = c.reservations.count/c.listings.count
+    end
+    ratio.max_by{|k,v| v}.first
+   end
+
+   def self.most_res
+     City.all.sort do |a,b|
+       a.reservations.count <=> b.reservations.count
+     end.last
+   end
 
 end
-
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,41 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :own_reservation
+  validate :listing_available?
+  validate :checkin_before_checkout?
+
+
+  def own_reservation
+    if listing.host_id == guest_id
+      errors.add(:guest, "Error")
+    end
+  end
+
+  def listing_available?
+    if checkin && checkout
+      listing.reservations.each do |r|
+        if r.checkin <= checkout && r.checkout >= checkin
+          errors.add(:reservation, "Error")
+        end
+      end
+    end
+  end
+
+  def checkin_before_checkout?
+    if checkin && checkout && checkout <= checkin
+      errors.add(:reservation, "Error")
+    end
+  end
+
+  def  duration
+    checkout - checkin
+  end
+
+  def total_price
+    listing.price*duration
+  end
 
 end
