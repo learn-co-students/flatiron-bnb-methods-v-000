c72225dbeb4d7e70b2e597f2114b368b16b11de9
Jesse Novotny <jessenovotny@gmail.com>
2016-08-24 10:47:48 -0700

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,22 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings starting, ending
+    binding.pry
+    # self.listings.map{|listing| listing.reservations}
+    reservations = []
+
+    self.listings.each do |listing|
+      reservations << listing.reservations
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+    binding.pry
+    map do |city|
+       city.listings.map{|listing| listing.reservations}.flatten.count / city.listings.flatten.count
+    end
+  end
+
 end
 
