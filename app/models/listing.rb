ca3f4bff099d63c2f9848c7a2558e9f6de9e1a5f
tpstar <hanlee12@gmail.com>
2016-08-25 00:18:30 -0400

    reservation 8 out of 17

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,23 @@ class Listing < ActiveRecord::Base
   has_many :guests, :class_name => "User", :through => :reservations
   validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
 
+  before_create :set_host_to_true
+  before_destroy :set_host_to_false
+
+  def set_host_to_true
+    self.host.host = true
+    self.host.save
+  end
+
+  def set_host_to_false
+    if self.host.listings.size < 2
+      self.host.host = false
+      self.host.save
+    end
+  end
+
+  def average_review_rating
+    self.reviews.inject{ |sum, review| sum.rating + review.rating }.to_f / self.reviews.size
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,15 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, :checkout, presence: true
+  validate :must_not_reserve_own_listing
+
+  def must_not_reserve_own_listing
+    if self.listing && self.listing.host && self.guest
+      # if self.listing.host_id != self.guest_id
+      #   errors.add(:listing, "must not reserve your own listing")
+      # end
+    end
+  end
 
 end
