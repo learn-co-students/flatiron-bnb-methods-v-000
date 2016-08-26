bbb6bc1fb66ccc039c515fd256fdab98084cf9ae
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-08-26 11:07:04 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,12 @@
+require 'pry'
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(start, fin)
+    list = Listing.all
+    
+    list.where("created_at > ?", Date.parse(start)) && list.where("created_at < ?", Date.parse(fin))
+  end
 
+end
