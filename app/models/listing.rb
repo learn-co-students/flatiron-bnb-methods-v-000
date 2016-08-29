aa2630f1539d30064607188aa771c86207f72366
bshap93 <benihana858@gmail.com>
2016-08-28 19:41:04 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,26 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood_id, presence: true
+
+  before_create :change_host_status
+  after_destroy :change_host_status, if: :all_gone?
+
+  protected
+  def change_host_status
+    self.host.host = !self.host.host
+    self.host.save
+  end
+
+  def all_gone?
+    self.host.listings.empty?
+  end
+
+
 end
