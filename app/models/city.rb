ea0950a144be2381c63c9a201e03f43085295e3d
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-08-30 09:55:43 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,11 +2,51 @@ require 'pry'
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
-  def city_openings(start, fin)
-    list = Listing.all
-    
-    list.where("created_at > ?", Date.parse(start)) && list.where("created_at < ?", Date.parse(fin))
+=begin
+  def city_openings(start_date, end_date)
+    available_listings = []
+    self.listings.each do |listing|
+      listing.reservations.each do |reservation|
+        if start_date.to_date >= reservation.checkout || end_date.to_date <= reservation.checkin
+          available_listings << listing
+        end
+      end
+    end
+    return available_listings.uniq
+  end
+=end
+  def city_openings(start_date, end_date)
+     self.listings.each do |listing|
+       listing.reservations.each do |res|
+         start_date <= res.checkout.to_s && end_date <= res.checkin.to_s
+       end
+     end
+  end
+
+  def self.highest_ratio_res_to_listings
+    @cities = []
+
+    all.each do |city|
+      if city.has_listings?
+        @cities << city
+      end
+    end
+
+    @cities.max_by do |city|
+      city.reservations.count / city.listings.count
+    end
+  end
+
+  def has_listings?
+    self.listings.any?
+  end
+
+  def self.most_res
+    self.all.max_by do |city|
+      city.reservations.count
+    end
   end
 
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,30 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  has_many :reservations, through: :listings
 
+  def neighborhood_openings(start_date, end_date)
+    self.listings.each do |listing|
+      listing.reservations.each do |reservation|
+        #reservation.where(checkin: start_date..end_date).empty? && reservation.where(checkout: start_date..end_date).empty?
+        reservation.checkin <= end_date.to_date && reservation.checkout >= start_date.to_date
+      end
+    end
+  end
+
+  def self.highest_ratio_res_to_listings
+    @neighborhoods = []
+
+    self.all.each do |hood|
+      if hood.listings.count > 0 && hood.reservations.count > 0
+        @neighborhoods << hood
+      end
+    end
+
+    @neighborhoods.max_by {|hood| hood.reservations.count / hood.listings.count}
+  end
+
+  def self.most_res
+    self.all.max_by {|hood| hood.reservations.count}
+  end
 end
