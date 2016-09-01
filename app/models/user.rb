0063ab15b6ac62a5e8bd240c330923957972bc2e
Ivan Pilot <ivanpilot@gmail.com>
2016-09-02 01:26:29 +0200

    improved user vlidation version

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,34 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
 
-  def guests
-    self.listings.collect do |listing|
-      listing.guests.each do |guest|
-        guest
-      end.uniq
-    end.flatten
-  end
+  ## As a guest
+  has_many :trip_listings, :through => :trips, :source => :listing
+  has_many :hosts, :through => :trip_listings, :foreign_key => :host_id
 
-  def hosts
-    self.trips.collect do |trip|
-      trip.listing.host
-    end
-  end
+  ## As a host
+  has_many :guests, :through => :reservations, :class_name => "User"
+  has_many :host_reviews, :through => :guests, :source => :reviews
 
-  def host_reviews
-    self.listings.collect do |listing|
-      listing.reviews.each do |review|
-        review
-      end
-    end.flatten
-  end
+  # def guests
+  #   self.listings.collect do |listing|
+  #     listing.guests.each do |guest|
+  #       guest
+  #     end.uniq
+  #   end.flatten
+  # end
+  #
+  # def hosts
+  #   self.trips.collect do |trip|
+  #     trip.listing.host
+  #   end
+  # end
+  #
+  # def host_reviews
+  #   self.listings.collect do |listing|
+  #     listing.reviews.each do |review|
+  #       review
+  #     end
+  #   end.flatten
+  # end
 
 end
