4b06bbb4427f91ae1ac4798eb584e8b7dbdef231
Ivan Pilot <ivanpilot@gmail.com>
2016-09-01 23:24:53 +0200

    all tests passed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,61 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :reservation_cannot_be_on_your_own_listing
+  validate :listing_must_be_available_at_checkin_before_making_reservation
+  validate :listing_must_be_available_at_checkout_before_making_reservation
+  validate :checkin_is_before_checkout
+  validate :checkin_and_checkout_are_different_dates
+
+  def openings(start_date, end_date)
+    self.checkin > end_date && self.checkout < start_date
+  end
+
+  def duration
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    self.duration * self.listing.price
+  end
+
+  private
+
+  #############_methods_for_validation############
+
+  def reservation_cannot_be_on_your_own_listing
+    if self.listing.host_id == self.guest_id
+      errors.add(:self, "cannot be on your own listing")
+    end
+  end
+
+  def listing_must_be_available_at_checkin_before_making_reservation
+    self.listing.reservations.each do |reservation|
+      if self.checkin && self.checkin.between?(reservation.checkin, reservation.checkout)
+        errors.add(:listing, "must be available at checkin before making reservation")
+      end
+    end
+  end
+
+  def listing_must_be_available_at_checkout_before_making_reservation
+    self.listing.reservations.each do |reservation|
+      if self.checkout && self.checkout.between?(reservation.checkin, reservation.checkout)
+        errors.add(:listing, "must be available at checkout before making reservation")
+      end
+    end
+  end
+
+  def checkin_is_before_checkout
+    if self.checkin && self.checkout && self.checkin > self.checkout
+      errors.add(:checkout, " must be after checkin")
+    end
+  end
+
+  def checkin_and_checkout_are_different_dates
+    if self.checkin && self.checkout && self.checkin == self.checkout
+      errors.add(:checkin, " and checkout can't be on the same day")
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,27 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+  def guests
+    self.listings.collect do |listing|
+      listing.guests.each do |guest|
+        guest
+      end.uniq
+    end.flatten
+  end
+
+  def hosts
+    self.trips.collect do |trip|
+      trip.listing.host
+    end
+  end
+
+  def host_reviews
+    self.listings.collect do |listing|
+      listing.reviews.each do |review|
+        review
+      end
+    end.flatten
+  end
+
 end
