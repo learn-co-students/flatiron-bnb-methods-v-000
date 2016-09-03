cb4585c4f32d6766035219728a5bcbd2703a97c9
miclp <michaelpeimani@gmail.com>
2016-09-03 16:41:47 -0700

    Add toggle user host status callback

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,14 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
 
-  validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+  validates :address, :listing_type, :title, :description, :price, :neighborhood, :host, presence: true
 
+  before_save :toggle_user_host_status
 
+  private
+
+  def toggle_user_host_status
+    self.host.host = true if self.host.host == false
+    self.host.save
+  end
 end
