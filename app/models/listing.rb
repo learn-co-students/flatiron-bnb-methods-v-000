ce11962cda89ac9590d367056b72de4e619a1978
miclp <michaelpeimani@gmail.com>
2016-09-03 16:55:24 -0700

    Change host status to false if no listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,10 +9,21 @@ class Listing < ActiveRecord::Base
 
   before_save :toggle_user_host_status
 
+  # using after_destroy. Might need before_destroy in order to see object attributes?
+  after_destroy :check_user_host_status
+
   private
 
   def toggle_user_host_status
     self.host.host = true if self.host.host == false
     self.host.save
+  end
+
+  # this is a little odd, the listing has been "destroyed" but I still have access to self.attributes. The if statement is therefor if the host has LESS THAN ONE listing (presumably because the last one just got destroyed?).
+  def check_user_host_status
+    if self.host.listings.size < 1 # presumably this listing WAS the last one
+      self.host.host = false
+      self.host.save
+    end
   end
 end
