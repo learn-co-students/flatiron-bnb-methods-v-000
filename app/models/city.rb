e2b54767b7d36add1be9a3ddda622f1e6ad43646
miclp <michaelpeimani@gmail.com>
2016-09-02 23:45:12 -0700

    Add #city_openings to City class

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,28 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(desired_checkin, desired_checkout)
+    problem_listings = []
+    # cycle through all the listings of self
+    self.listings.each do |listing|
+      listing.reservations.each do |reservation|
+        if reservation_conflict?(reservation, desired_checkin, desired_checkout)
+          problem_listings << listing unless problem_listings.include?(listing)
+        end
+      end
+    end
+    openings = self.listings.all - problem_listings
+  end
 
+  def reservation_conflict?(reservation, ckin, ckout)
+    # return true if there is a conflict
+    desired_checkin = ckin.to_date
+    desired_checkout = ckout.to_date
+    if reservation.checkout > desired_checkin && reservation.checkout < desired_checkout
+      return true
+    elsif reservation.checkin > desired_checkin && reservation.checkin < desired_checkin
+      return true
+    end
+    return false
+  end
+end
