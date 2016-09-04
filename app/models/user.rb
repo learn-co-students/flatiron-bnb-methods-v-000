d1c15f272fb013fb20399f7abb8a4a327a60d341
miclp <michaelpeimani@gmail.com>
2016-09-03 22:03:22 -0700

    Add associations between guests and hosts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+  has_many :guests, :class_name => "User", :through => :reservations
+
+  # has_many :hosts, :class_name => "User", :through => :reservations, :source => :listings
 end
