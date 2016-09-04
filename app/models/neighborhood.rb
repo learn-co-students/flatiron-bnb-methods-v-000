993b074aa875c2d8354c3112d7ee985d4d7e6964
miclp <michaelpeimani@gmail.com>
2016-09-03 18:50:01 -0700

    Add reservation ratio and count methods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -26,4 +26,35 @@ class Neighborhood < ActiveRecord::Base
     return false
   end
 
+  # class methods
+
+  def self.highest_ratio_res_to_listings
+    ratios = {}
+    self.res_counts.each do |name, num_res|
+      num_listings = self.find_by(name: name).listings.size
+      if num_listings == 0
+        ratios[name] = 0
+      else
+        ratios[name] = num_res/num_listings
+      end
+    end
+    self.find_by(name: ratios.max_by {|k,v| v}[0])
+  end
+
+  def self.most_res
+    self.find_by(name: self.res_counts.max_by {|k, v| v}[0])
+  end
+
+  # returns a hash with neighborhood names as keys and the number of reservations as values
+  def self.res_counts
+    res_count = {}
+    self.all.each do |neighb|
+      count = 0
+      neighb.listings.each do |listing|
+        count += listing.reservations.size
+      end
+      res_count[neighb.name] = count
+    end
+    res_count
+  end
 end
