7b65abf62b749833b9f5d8ad2dd2302b61efa594
dannyenfp <daniel.valmas@gmail.com>
2016-09-12 13:42:31 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,36 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
+
+
+  def city_openings(start_date, end_date)
+    self.listings.each do |listing|
+      listing.reservations.each do |res|
+        start_date <= res.checkout.to_s && end_date <= res.checkin.to_s
+      end
+    end
+  end 
+
+  def ratio_res_to_listings
+    if listings.count > 0
+      reservations.count / listings.count
+    else
+      0
+  end 
+  end 
+
+  def self.highest_ratio_res_to_listings
+  self.all.max do |a, b|
+      a.ratio_res_to_listings <=> b.ratio_res_to_listings
+    end
+  end 
+
+  def self.most_res
+    self.all.max do |a, b|
+      a.reservations.size <=> b.reservations.size
+    end
+  end
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,41 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+
+  validates_presence_of :checkin, :checkout
+  validate :guest_is_not_host, :listing_available, :checkin_before_checkout
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
+
+  private
+
+    def guest_is_not_host
+      if guest_id == listing.host_id
+        errors.add(:guest_id, "You cannot book your own listing.")
+      end
+    end
+
+    def listing_available
+      Reservation.where(listing_id: listing_id).where.not(id: id).each do |res|
+        dates_reserved = res.checkin..res.checkout
+        if dates_reserved === checkin || dates_reserved === checkout
+          errors.add(:guest_id, "Sorry, this listing is not available during those dates.")
+        end
+      end
+    end
+
+    def checkin_before_checkout
+      if checkin && checkout && checkout <= checkin
+        errors.add(:guest_id, "Checking date must be before checkin.")
+      end
+    end
+
+
+
 end
