d2eabacc96c95aedb1aa68adf499ff1891a51a63
ed-karabinus <ejkarabin@gmail.com>
2016-09-22 10:17:58 -0500

    Finished Flatiron Bnb Methods Lab.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates_presence_of :rating, :description, :reservation
+  validate :occurred_after_checkout, if: "reservation && created_at"
 
+  def occurred_after_checkout
+    if (reservation.checkout >= created_at.to_date)
+      errors.add(:reservation, "can't be ongoing at time of review")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+  has_many :guests, :through => :reservations
+  has_many :host_reviews, :through => :reservations, :source => :review
+  has_many :destinations, :through => :trips, :source => :listing
+  has_many :hosts, :through => :destinations
 end
