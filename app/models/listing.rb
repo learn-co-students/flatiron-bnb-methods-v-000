2a5758652258559acfa3ca8146394815a17a5a1f
ed-karabinus <ejkarabin@gmail.com>
2016-09-21 19:21:16 -0500

    Added validations and callbacks to Listing model definition. Callbacks not working as intended at this time.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,13 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood_id
+
+  after_create do
+    self.host.host = true
+  end
+
+  before_destroy do
+    self.host.host = false
+  end
 end
