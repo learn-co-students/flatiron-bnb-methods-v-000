35a8617d8b2b07cb189d83cd4d32e8eefac26943
tylertaylor <vikingtyty@gmail.com>
2016-09-26 18:36:00 -0500

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,13 +7,37 @@ class Reservation < ActiveRecord::Base
 
   validate :available, :guest_not_host
 
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    self.listing.price * duration
+  end
+
   private
 
   def available
-    binding.pry
-    Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
-      binding.pry
+    res = Reservation.where(listing_id: listing.id)
+    if checkin && checkout
+      # iterate through res and check if id = res.id?
+      res.each do |r|
+        if id == r.id
+          break
+        elsif checkin.between?(r.checkin, r.checkout) || checkout.between?(r.checkin, r.checkout)
+          errors.add(:guest_id, "Sorry, this place isn't available during the date you specified.")
+        elsif checkout <= checkin
+          errors.add(:guest_id, "Checkin must be before checkout.")
+        end
+      end
     end
+
+    # Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+    #   booked_dates = (r.checkin..r.checkout) # create range of booked dates
+    #   if booked_dates === checkin || booked_dates === checkout # If checkin/checkout dates are within the range of booked dates
+    #     errors.add(:guest_id, "Sorry, this place isn't available during the dates you specified.")
+    #   end
+    # end
   end
 
   def guest_not_host
