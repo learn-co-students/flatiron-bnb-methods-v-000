ce941a5d8806d9dd44a0818064f4a7d4b0707c4d
DownsideupmediaUSA <brandondecarlo@live.com>
2016-10-01 17:39:13 -0400

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,16 @@
 class Review < ActiveRecord::Base
-  belongs_to :reservation
-  belongs_to :guest, :class_name => "User"
+belongs_to :guest, :class_name => "User"
+belongs_to :reservation
+
+validates_presence_of :rating, :description, :reservation
+
+
+validate :reservation_exists_and_accepted_hasnt_happened_yet
+
+  private
+  #You can't write a review on a reservation that doesn't exist
+  def reservation_exists_and_accepted_hasnt_happened_yet
+    errors.add(:reservation_id, "doesn't exist") unless Reservation.exists?(reservation_id) && Reservation.find(reservation_id).status == "accepted" && Reservation.find(reservation_id).checkout < Date.today
+  end
 
 end
