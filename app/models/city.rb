99eb62b1b2325c4dc716c2fa8232359f201a3bd0
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-03 06:23:42 +0000

    initial commit

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,29 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(date_1, date_2)
+    Listing.all
+  end
 
+  def self.highest_ratio_res_to_listings
+    # Reservation.all <=> Listing.all
+    # find the city with the highest ratio of reservations to listings
+    l = Listing.find_by(:name).maximum(:reservations)
+    c = City.find_by(:name).maximum(:listings)
+
+  end
+
+  private
+    # def greater_than
+    #   r = 0
+    #   self.listing.all.each do |l|
+    #     if l.reservations > l
+    #        r +=1
+    #      end
+    #      r
+    #   end
+    #
+    # end
+
+
+end
