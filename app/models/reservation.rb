517fa78a89cf4a98705ef56b12b7e27d2638f735
Traci7822 <Tracir@gmail.com>
2016-10-03 16:09:06 -0600

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,6 +5,7 @@ class Reservation < ActiveRecord::Base
   validates :checkin, presence: true
   validates :checkout, presence: true
   validate :not_own_reservation?
+  validate :listing_available_at_checkin?
 
   def not_own_reservation?
     if self.listing.host_id == self.guest_id
@@ -12,5 +13,13 @@ class Reservation < ActiveRecord::Base
     end
   end
 
+  def listing_available_at_checkin?
+    listing.reservations.each do |r|
+      binding.pry
+      if checkin.to_s.between?(r.checkin.to_s, r.checkout.to_s)
+        errors.add(:reservation, "Reservation is not available for this checkin date")
+      end
+    end
+  end
 
 end
