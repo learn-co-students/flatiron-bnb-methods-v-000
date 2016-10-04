f492c6d6b275fc61cc3f9babb901ef46ae7cc68e
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-04 01:42:54 +0000

    Completed the first class method

--- app/models/city.rb
+++ app/models/city.rb
@@ -8,30 +8,11 @@ class City < ActiveRecord::Base
   end
 
   def self.highest_ratio_res_to_listings
-    # Reservation.all <=> Listing.all
-    # find the city with the highest ratio of reservations to listings
-    # city_names = City.all.group(:name)
-    # listing_names = Listing.all.group(:name)
-    # City.find_by
       hash ={}
-      City.all.map do |city|
+      City.all.map.max_by do |city|
         hash["#{city.name}"] = city.reservations
-        hash
+        hash["#{city.name}"][-1]
       end
-
-      highest = nil
-      hash.max_by do |key, array|
-         hash[key][-1]
-      end
-      # c_l = Reservation.all.map { |c| c.listing  }
-      # l_r = Listing.all.each { |l| l.reservations }
-      # c_l_count / l_r_count
-    # c = City.find_by(:name).maximum(:listings)
-    # expect(City.highest_ratio_res_to_listings).to eq(City.find_by(:name => "Denver"))
-    # find city with the highest reservations
-    # count the reservations for each city
-    # store each city's reservation in array called ccity reservation
-    # then count each array and whichever array has the highest count it's city's name will be the answer.
   end
 
   private
