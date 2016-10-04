6d72899e432a967a0551d9ed17d5a840045529b8
Traci7822 <Tracir@gmail.com>
2016-10-03 18:46:53 -0600

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,6 +6,8 @@ class Reservation < ActiveRecord::Base
   validates :checkout, presence: true
   validate :not_own_reservation?
   validate :listing_available_at_checkin?
+  validate :checkin_before_checkout?
+  validate :checkin_is_not_checkout
 
   def not_own_reservation?
     if self.listing.host_id == self.guest_id
@@ -14,11 +16,33 @@ class Reservation < ActiveRecord::Base
   end
 
   def listing_available_at_checkin?
-    Reservation.where(listing_id: listing_id).where.not(id: id).each do |r|
-      if checkin.to_s.between?(r.checkin.to_s, r.checkout.to_s)
+    listing.reservations.each do |r|
+      booked_dates = r.checkin..r.checkout
+      if booked_dates === checkin || booked_dates === checkout
         errors.add(:checkin, "Reservation is not available for this checkin date")
       end
     end
+  end
+
+  def checkin_before_checkout?
+    if checkin.to_s <= checkout.to_s
+    else
+      errors.add(:checkout, "Check-in date must be before Check-out date")
+    end
+  end
+
+  def checkin_is_not_checkout
+    if checkin == checkout
+      errors.add(:samedates, "Can not check in and out on the same day")
+    end
+  end
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    self.listing.price.truncate(2) * duration
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,11 @@ class Review < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   validates :rating, presence: true
   validates :description, presence: true
+  validate :associated_reservation
+
+  def associated_reservation
+    unless self.reservation != nil && self.reservation.status == "accepted" && self.reservation.checkout <= Date.today
+      errors.add(:review, "Requirements have not been met to leave a review")
+    end
+  end
 end
