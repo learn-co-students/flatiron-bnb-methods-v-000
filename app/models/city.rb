6ebe5926af82573bcd4c2f37c3d31d6191e9ab2f
Lenn Hypolite <lenntriniman@hotmail.com>
2016-10-04 16:14:15 +0000

    error type mismatch

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,29 +3,23 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
   has_many :reservations, :through => :listings
 
+  validates :name, presence: true
+
   def city_openings(date_1, date_2)
     Listing.all
   end
 
   def self.highest_ratio_res_to_listings
       hash ={}
-      City.all.map.max_by do |city|
+      self.all.map.max_by do |city|
         hash["#{city.name}"] = city.reservations
         hash["#{city.name}"][-1]
       end
   end
 
+  def self.most_res
+    self.highest_ratio_res_to_listings
+  end
   private
-    # def greater_than
-    #   r = 0
-    #   self.listing.all.each do |l|
-    #     if l.reservations > l
-    #        r +=1
-    #      end
-    #      r
-    #   end
-    #
-    # end
-
 
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,26 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+
+  validates :address,:title, :description, :price, :listing_type, :neighborhood, presence: true
+  #
+  after_save :set_to_true
+  # before_destroy :set_to_false
+
+    def set_to_true
+      # binding.pry
+      # listing = Listing.find_by(host_id: false)
+      # listing = Listing.where("host_id = ?", @host.id)
+      binding.pry
+       listing = Listing.new
+        listing.host = true
+      
+    end
+    #
+    # def set_to_false
+    #   Listing.new.host = false
+    # end
+
+
 end
