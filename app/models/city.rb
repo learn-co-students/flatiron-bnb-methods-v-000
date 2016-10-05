da967594bcd8aa8e7c80d8e5f49233c245e80ef7
johnsontaiwo <josseytee@gmail.com>
2016-10-05 19:23:56 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,25 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  
+
+  def city_openings(start_date, end_date)
+    @listings = Listing.all
+
+  end
+
+  def self.highest_ratio_res_to_listings
+     @cities = City.all
+     @cities.max_by do |city|
+      city.name
+    end
+      
+  
+   
+  end
+
+
+
 
 end
 
