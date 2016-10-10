848700309809a8ca31d037a141c83eebc67089f5
johnsontaiwo <josseytee@gmail.com>
2016-10-10 10:58:25 -0400

    modified files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
-  has_many :listings, :foreign_key => 'host_id'
-  has_many :reservations, :through => :listings
+  has_many :listings, :foreign_key => 'host_id', dependent: :destroy
+  has_many :reservations, :foreign_key => 'host_id', :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+  has_many :guests, :through => :listings
+  #has_many :hosts, :through => :trips, :class_name => "Reservation"
+  has_many :host_reviews, :through => :guests, :source => :reviews
+  has_many :trip_listings, :through => :trips, :source => :listing
+has_many :hosts, :through => :trip_listings, :foreign_key => "host_id"
+
+   #def guests
+
 end
