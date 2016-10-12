cbd9f43eb29e8083c57e22c82f9f4660ea14b9cf
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-12 19:11:03 -0400

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,18 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(startdate, enddate)
+    binding.pry
+    results = [] 
+    self.listings.each do |listing|
+      listing.reservations.each do |rsvp|
+        results << listing if rsvp.checkin > Date.parse(startdate) && rsvp.checkout < Date.parse(enddate)
+      end
+    end
+    results
+  end
+
+# @city.city_openings('2014-05-01', '2014-05-05')
+
 end
 
