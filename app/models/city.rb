a68cfd5beb02d59be23c789c3ba8e87200bace62
Rebecca <beckysherman000@gmail.com>
2016-10-15 21:28:52 -0400

    Pass city and neighborhood specs, add module available

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,12 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
-  has_many :listings, :through => :neighborhoods
+  has_many :listings, through: :neighborhoods
+  has_many :reservations, through: :listings
 
+  include Available::InstanceMethods
+  extend  Available::ClassMethods
+
+  def city_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
 end
-
