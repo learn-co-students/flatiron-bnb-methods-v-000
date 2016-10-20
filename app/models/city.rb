e2508c9125b0702dd1207f10ceb090e564a82f80
lizdenhup <lizdenhup@gmail.com>
2016-10-20 15:20:46 -0500

    #city_openings works

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,17 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(start_date, end_date)
+    available_reservations = []
+    listings.each do |listing|
+      available = listing.reservations.all? do |res|
+        Date.parse(start_date) >= res.checkout || Date.parse(end_date) <= res.checkin
+      end
+      if available 
+        available_reservations << listing
+      end 
+    end
+    available_reservations
+  end  
 
+end
