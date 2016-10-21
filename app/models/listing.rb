1765b5964807d9da59b7a92fe9411aff1f6858ca
lizdenhup <lizdenhup@gmail.com>
2016-10-20 19:05:51 -0500

    added ar callbacks for listings model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,25 @@ class Listing < ActiveRecord::Base
   has_many :guests, :class_name => "User", :through => :reservations
   
   validates_presence_of :address, :listing_type, :title, :description, :price, :neighborhood
+
+  after_save :set_user_to_host
+
+  before_destroy :user_is_no_longer_host
+
+  private
+
+  def set_user_to_host
+    self.host.update(:host => true)
+    #host is a method that listing can access, listing
+    #cannot access a .user method so that is why we call
+    #self.host and then set the host boolean in the user
+    #migration to true
+  end 
+
+  def user_is_no_longer_host
+    if self.host.listings.count == 1
+      self.host.update(:host => false)
+    end 
+  end 
+
 end
