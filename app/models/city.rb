9cea9fc04ea087bfe522c67c56c972da8e0aeb6c
Clare <clare.e.horton@gmail.com>
2016-11-08 17:42:14 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,28 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
-
+  
+  def city_openings(start_date, end_date)
+    parse_overlap(start_date, end_date)
+  end
+  
+  def self.highest_ratio_res_to_listings
+    
+  end
+  
+  def self.most_res
+    self.all.max do |city|
+    end
+  end
+  
+  private
+  
+  def parse_overlap(start_date, end_date)
+    self.listings.select do |listing|
+      listing.reservations.none? do |reservation|
+        (Date.parse(start_date) <= reservation.checkout) && (Date.parse(end_date) >= reservation.checkin) 
+      end
+    end
+  end
 end
 
