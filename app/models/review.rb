b592a82f9bf5b26f5fb472c956c79215296b6eff
AliceKB <alicekristinabalbuena@gmail.com>
2016-11-07 23:30:54 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,21 @@
 class Review < ActiveRecord::Base
+  validates :rating,:description, :reservation, presence: true
+  validate :checkout, :reservation_accepted
+
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  private
+
+  def checkout
+    if reservation && reservation.checkout > Date.today
+      errors.add(:reservation, "Reservation must have ended.")
+    end
+  end
+
+  def reservation_accepted
+    if reservation.try(:status) != 'accepted'
+      errors.add(:reservation, "Revervation must be valid")
+    end
+  end
 end
