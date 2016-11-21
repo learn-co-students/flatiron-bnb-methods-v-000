1d1822435336169494b0cdc10c9a2d970de04691
hilaryml <hilyttle@gmail.com>
2016-11-21 14:24:34 -0500

    Done.

--- app/models/concerns/available.rb
+++ app/models/concerns/available.rb
@@ -1,9 +1,8 @@
 module Available
-  extend ActiveSupport::Concern
 
   module InstanceMethods
     def openings(date1, date2)
-      Listing.all.select do |listing|
+      listings.select do |listing|
         listing.reservations.each do |reservation|
           ((reservation.checkin.to_date)..(reservation.checkout.to_date)) == ((date1.to_date)..(date2.to_date))
         end
@@ -11,8 +10,6 @@ module Available
     end
 
     def ratio_res_to_listings
-      #@objects.max { |a, b| (a.reservations.count.to_f/a.listings.count.to_f) <=> (b.reservations.count/b.listings.count)}
-
       if listings.count > 0
         reservations.count.to_f / listings.count.to_f
       else
