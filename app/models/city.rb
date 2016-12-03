3ceb2f928b08be91f8ed2141eca524eaf299e8f6
BrunoBoehm <bruno.boehm@gmail.com>
2016-12-03 14:32:31 +0100

    blocking on the SQL merge for listings available at a certain date

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,42 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
+  def city_openings(start_date, end_date)
+  	self.listings.merge( Listing.bookable(start_date, end_date) )
+  	# intersect the city's (neighborhood's) listings with bookable listings
+  	# City.first.city_openings('2014-05-01', '2014-05-05')
+  	# will SQL query
+  	#
+  	# v1
+  	# SELECT "listings".* FROM "listings" 
+  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
+  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
+  	# 	WHERE "neighborhoods"."city_id" = ? 
+  	# 	AND (NOT (checkin <= '2014-05-05' AND checkout >= '2014-05-01'))
+  	#   [["city_id", 1]]
+  	#
+  	# v2
+  	# SELECT "listings".* FROM "listings" 
+  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
+  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
+  	# 	WHERE "neighborhoods"."city_id" = ? 
+  	# 	AND (NOT ("reservations"."checkin" BETWEEN '2014-05-01' AND '2014-05-05')) 
+  	# 	AND (NOT ("reservations"."checkout" BETWEEN '2014-05-01' AND '2014-05-05'))  
+  	# 	[["city_id", 1]]
+  	#
+  	# v3
+  	# SELECT "listings".* FROM "listings" 
+  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
+  	# 	WHERE (NOT ("reservations"."checkin" BETWEEN '2014-05-01' AND '2014-05-05'))
+  	# SELECT "listings".* FROM "listings" 
+  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
+  	# 	WHERE (NOT ("reservations"."checkout" BETWEEN '2014-05-01' AND '2014-05-05'))
+  	# SELECT "listings".* FROM "listings" 
+  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
+  	# 	WHERE "neighborhoods"."city_id" = ?  
+  	# 	[["city_id", 1]]		
+  end
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,22 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+
+  def self.bookable(start_date, end_date)
+  	if start_date && end_date
+	  	joins(:reservations).where.not(reservations: {checkin: start_date..end_date}) +
+	    joins(:reservations).where.not(reservations: {checkout: start_date..end_date})
+    else
+        []
+    end
+  	# v2 was
+ 	# period = start_date..end_date
+	# no_checkin_in_period = Reservation.where.not(checkin: period)
+	# no_checkout_in_period = Reservation.where.not(checkout: period)
+	# self.joins(:reservations).merge(no_checkin_in_period).merge(no_checkout_in_period)
+	#
+	# v1 was
+	# self.joins(:reservations).merge( Reservation.where.not("checkin <= ? AND checkout >= ?", end_date, start_date) )
+	# listings that have reservations, AND these reservations are not overlapping with the start and end date
+  end
 end
