cf1f061acbc90f64a62db57bc0c338c2a7faab7d
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-12-04 17:47:47 -0800

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,48 @@
 class Listing < ActiveRecord::Base
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
+
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood, presence: true
+
+  after_save :make_host
+
+  before_destroy :remove_host
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  private
+
+  def self.available(start_date, end_date)
+    if start_date && end_date
+      joins(:reservations).
+        where.not(reservations: {check_in: start_date..end_date}) &
+      joins(:reservations).
+        where.not(reservations: {check_out: start_date..end_date})
+    else
+      []
+    end
+  end
+
+  def remove_host
+    if Listing.where(host: host).where.not(id: id).empty?
+      host.update(is_host: false)
+    end
+  end
+
+  def make_host
+    unless host.is_host?
+      host.update(is_host: true)
+    end
+  end
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,9 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  include Reserve
+
+  def neighborhood_openings(start_date, end_date)
+    openings(start_date, end_date)
+  end
 end
