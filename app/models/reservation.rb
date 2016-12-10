2138c24d67f598ba711fec5ac2bbd3a2507a2a97
Dom McKellar <dom.mckellar@gmail.com>
2016-12-10 07:36:12 -0800

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,37 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :check_in, :check_out, presence: true
+  validate :available, :check_out_after_check_in, :guest_and_host_not_the_same
+
+  def duration
+    (check_out - check_in).to_i
+  end
+
+  def total_price
+    listing.price * duration
+  end
+
+  private
+  
+  def available
+    Reservation.where(listing_id: listing.id).where.not(id: id).each do |r|
+      booked_dates = r.check_in..r.check_out
+      if booked_dates === check_in || booked_dates === check_out
+        errors.add(:guest_id, "Sorry, this place isn't available during your requested dates.")
+      end
+    end
+  end
+
+  def guest_and_host_not_the_same
+    if guest_id == listing.host_id
+      errors.add(:guest_id, "You can't book your own apartment.")
+    end
+  end
+
+  def check_out_after_check_in
+    if check_out && check_in && check_out <= check_in
+      errors.add(:guest_id, "Your check-out date needs to be after your check-in.")
+    end
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,28 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :description, presence: true
+  validates :rating, presence: true, numericality: {
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 5,
+              only_integer: true
+            }
+  validates :reservation, presence: true
+
+  validate :checked_out
+  validate :reservation_accepted
+
+  private
+
+  def checked_out
+    if reservation && reservation.check_out > Date.today
+      errors.add(:reservation, "Reservation must have ended to leave a review.")
+    end
+  end
+
+  def reservation_accepted
+    if reservation.try(:status) != 'accepted'
+      errors.add(:reservation, "Reservation must be accepted to leave a review.")
+    end
+  end
 end
