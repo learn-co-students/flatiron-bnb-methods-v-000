bb1e833793852e877aa34a3997f044f5fec98509
andresduarte <finnleyam@gmail.com>
2016-12-13 00:18:36 -0500

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,45 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(date_1, date_2)
+    start_date = Date.parse(date_1)
+    end_date = Date.parse(date_2)
+    available_listings = []
+
+    listings.each do |listing|
+      unless listing.reservations.any? { |res| start_date.between?(res.checkin, res.checkout) || end_date.between?(res.checkin, res.checkout) }
+        available_listings << listing
+      end
+    end
+    available_listings
+  end
+
+  def self.highest_ratio_res_to_listings
+    city_res_count = 0
+    high_ratio = nil
+    self.all.each do |city|
+      city.listings.each do |listing|
+        if city_res_count < listing.reservations.count
+          city_res_count = listing.reservations.count
+          high_ratio = city
+        end
+      end
+    end
+    high_ratio
+  end
+
+  def self.most_res
+    city_res_count = 0
+    most_res = nil
+    self.all.each do |city|
+      city.listings.each do |listing|
+        if city_res_count < listing.reservations.count
+          city_res_count = listing.reservations.count
+          most_res = city
+        end
+      end
+    end
+    most_res
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,40 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+
+  validate :reserve_own_listing, :check_availability, :checkin_comes_first
+
+  def duration
+    (checkout - checkin).to_f
+  end
+
+  def total_price
+    (listing.price * duration).to_f
+  end
+
+  private
+
+    def reserve_own_listing
+      # guest id and host id can't be the same
+      if self.guest_id == listing.host_id
+        errors.add(:reserve_own_listing, "You can't reserve your own listing")
+      end
+    end
+
+    def check_availability
+      listing.reservations.each do |res|
+        taken_dates = (res.checkin..res.checkout)
+        if taken_dates === checkin || taken_dates === checkout
+          errors.add(:guest_id, "Unavailable.")
+        end
+      end
+    end
+
+    def checkin_comes_first
+      if (checkin.present? && checkout.present?) && checkin >= checkout
+        errors.add(:guest_id, "Check-In cannot come after Check-Out.")
+      end
+    end
+
 end
