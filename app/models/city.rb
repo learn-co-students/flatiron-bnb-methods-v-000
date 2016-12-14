e820815312614b0aab7938206ece7c06d31d6755
BriOD <briodonoghue@gmail.com>
2016-12-14 16:04:49 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,14 @@
 class City < ActiveRecord::Base
+  include Ratio::InstanceMethods
+  extend Ratio::ClassMethods
+
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
+
+  def city_openings(start_date, end_date)
+    self.listings.select {|listing| !listing.booked?(start_date, end_date)}
+  end
+
 
 end
-
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,41 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, :checkout, presence: true
+  validate :guest_not_host, :checkin_available, :checkout_available, :checkin_not_checkout
+
+  def duration
+    (checkout - checkin).to_i
+  end
+
+  def total_price
+    listing.price.to_f * duration
+  end
+
+  private
+
+  def guest_not_host
+    if self.guest == self.listing.host
+      errors.add(:guest, "cannot make a reservation on your own listing")
+    end
+  end
+
+  def checkin_available
+    if checkin && checkout && (listing.booked?(checkin, checkin) || checkin > checkout)
+      errors.add(:checkin, "invalid checkin date")
+    end
+  end
+
+  def checkout_available
+    if checkout && listing.booked?(checkout, checkout)
+      errors.add(:checkout, "invalid checkout date")
+    end
+  end
+
+  def checkin_not_checkout
+    if checkin == checkout
+      errors.add(:checkout, "checkout must be after checkin")
+    end
+  end
+
 end
