4c797f2264c44e3efa5b8e9c572cd3f6ed6864f6
Laurent Milne <lmilne360@gmail.com>
2016-12-20 02:41:42 +0000

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,16 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, :description, presence: true
+
+  validate :check_validity
+
+  private
+
+    def check_validity
+      if !reservation || reservation.status != 'accepted' || reservation.checkout > Date.today
+        errors.add(:check_validity, "Review is Invalid")
+      end
+    end
+
 end
