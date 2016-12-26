a61392cf971d8bc5b14d6648387eab23b2883b8d
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-26 18:35:20 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,22 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-end
+  def city_openings(s_date, e_date)
+    s_date = Date.parse(s_date)
+    e_date = Date.parse(e_date)
 
+    open_listings = []
+
+    self.listings.map do |listing|
+       if listing.reservations.none? do |reservation|
+             #binding.pry
+             range = reservation.checkin..reservation.checkout
+            (range === s_date) || (range === e_date)
+          end
+          open_listings << listing
+        end
+      end
+      open_listings
+  end
+
+end
