8f8298b27b101578baa29b2e8fc07b16686bbfa4
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-26 19:01:53 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,16 +3,15 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(s_date, e_date)
-    s_date = Date.parse(s_date)
-    e_date = Date.parse(e_date)
+
+    input = Date.parse(e_date)..Date.parse(s_date)
 
     open_listings = []
 
-    self.listings.map do |listing|
+    self.listings.each do |listing|
        if listing.reservations.none? do |reservation|
-             #binding.pry
              range = reservation.checkin..reservation.checkout
-            (range === s_date) || (range === e_date)
+             range.overlaps?(input)
           end
           open_listings << listing
         end
