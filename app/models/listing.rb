2c88ff3091ad1bfb46b4eaea646fd0266cdbf312
msosborne3 <msosborne@ymail.com>
2017-01-04 14:43:54 -0600

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,41 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  # Listing is only valid if it has an address, listing_type, title,
+  # description, price, and neighborhood_id
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :neighborhood_id, presence: true
+
+  after_create :toggle_host_status
+  after_destroy :toggle_host_status
+
+
+
+  # Returns true if a listing has an opening within a range of dates.
+  def available?(start_date, end_date)
+    unless self.reservations.any? { |res| start_date.to_date.between?(res.checkin, res.checkout) || end_date.to_date.between?(res.checkin, res.checkout)} 
+      true
+    end
+  end
+
+  # Averages all of the reviews for a listing
+  def average_review_rating
+    self.reviews.all.average(:rating)
+  end
+
+  private 
+
+  # When a user creates a listing, they become a host, and when they destroy all listings, they are no longer a host.
+  def toggle_host_status
+    if !self.host.listings.empty?
+      self.host.update(host: true)
+    else
+      self.host.update(host: false)
+    end
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,48 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
+  validate :host_guest_different, :available_during_stay, :checkin_before_checkout
+
+  # Duration is calculated by subtracting the checkin date from the checkout date
+  def duration
+    if checkin && checkout
+      duration = checkout - checkin
+    end
+    duration
+  end
+
+  # The total price is calculated by multiplying the listings price per night by the
+  # number of nights (duration)
+  def total_price
+    total_price = 0
+
+    total_price = listing.price * duration
+  end
+
+  private
+
+  # Custom validation to ensure that that the host cannot reserve their own home
+  def host_guest_different
+    if listing.host_id == guest_id
+      errors.add(:guest_id, "You cannot reserve your own place.")
+    end
+  end
+
+  # Custom validation ensures that a listing is available for the requested dates
+  def available_during_stay
+    if checkin && checkout && !listing.available?(checkin, checkout)
+      errors.add(:guest_id, "Unfortunately, this place isn't available for the dates you requested")
+    end
+  end
+
+  # Custom validation ensures that a checkout date must come after a checkin date
+  def checkin_before_checkout
+    if checkin && checkout && checkout <= checkin
+      errors.add(:guest_id, "You cannot checkout before checking in.")
+    end
+  end
+
 end
