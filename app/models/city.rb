e838e935672428e37fef5ac30d7de3faf0ea78c3
William Story <wstory@villanova.edu>
2017-01-06 23:26:32 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,9 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(start, finish)
-    self.listings.reservations.where('checkin < ? AND checkout > ?', start, finish)
+    Listing.all.each do |list|
+      list.reservations.where('checkin < ? AND checkout > ?', start, finish)
+    end
   end
 
   def self.highest_ratio_res_to_listings
