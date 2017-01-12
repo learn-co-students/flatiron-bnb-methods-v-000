9de5961d01f520303a44f183b3aa4be4ca46169f
Bill <storywr@gmail.com>
2017-01-11 19:53:40 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,15 @@ class Review < ActiveRecord::Base
 
   validates :rating, presence: true
   validates :description, presence: true
+
+  validate :confirm
+
+  def confirm
+    if self.reservation && self.reservation.checkout <= Date.today
+      true
+    else
+      errors.add(:description, "Can't write Review!")
+    end
+  end
+
 end
