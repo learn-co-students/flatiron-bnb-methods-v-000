e6bdb5a12898ec481871c7ce64efc3a380acfed3
Scott Savage <snsavage@gmail.com>
2017-01-17 16:23:14 -0500

    Add City#city_openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,14 @@ class City < ActiveRecord::Base
   has_many :listings, :through => :neighborhoods
 
   def city_openings(start_date, end_date)
-    self.listings.all
+    start_date = Date.parse(start_date)
+    end_date = Date.parse(end_date)
+
+    listings.includes(:reservations).select do |listing|
+      listing.reservations.none? do |reservation|
+        reservation.checkin <= end_date && start_date <= reservation.checkout
+      end
+    end
   end
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,12 +4,4 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-
-  # def overlapping_reservations
-  #   listing.reservations.where(
-  #     "checkin <= :end_date AND :start_date <= checkout",
-  #     start_date: checkin,
-  #     end_date: checkout,
-  #   )
-  # end
 end
