a23f5885236483a9fb6266c1223de56dd01183ae
Scott Savage <snsavage@gmail.com>
2017-01-17 16:59:42 -0500

    Pass all City specs

--- app/models/city.rb
+++ app/models/city.rb
@@ -12,5 +12,30 @@ class City < ActiveRecord::Base
       end
     end
   end
+
+  def self.highest_ratio_res_to_listings
+    cities = City.includes(listings: [:reservations]).all
+
+    cities.collect do |city|
+      total_listings = city.listings.count
+      total_reservations = city.listings.reduce(0) do |memo, listing|
+        memo += listing.reservations.count
+      end
+
+      {city: city, ratio: total_reservations / total_listings.to_f}
+    end.max_by { |x| x[:ratio] }[:city]
+  end
+
+  def self.most_res
+    cities = City.includes(listings: [:reservations]).all
+
+    cities.collect do |city|
+      total_res = city.listings.reduce(0) do |memo, listing|
+        memo += listing.reservations.count
+      end
+
+      {city: city, total_res: total_res}
+    end.max_by { |x| x[:total_res] }[:city]
+  end
 end
 
