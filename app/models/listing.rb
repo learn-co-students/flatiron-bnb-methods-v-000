0f1366edb10aaa4aca99fe466cc31c580c804773
Krishel Lasam <krishel.lasam@gmail.com>
2017-01-31 01:59:37 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,20 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  after_create :host_status
+  after_destroy :host_status
+
+  def host_status
+    if host.listings.empty?
+      host.update(host: false)
+    else
+      host.update(host: true)
+    end
+  end
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates :rating, :description, :reservation, presence: true
+  validate :review
 
+  def review
+    unless reservation && reservation.status == "accepted" && reservation.checkout < Date.today
+      errors.add(:reservation, "Must have a valid reservation")
+    end
+  end
 end
