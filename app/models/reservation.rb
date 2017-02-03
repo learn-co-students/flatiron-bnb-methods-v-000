bcfd0b7d304d80fa5d2bba8ed0a7317d97c0eef3
Senad Hadrovic <github email address>
2017-02-02 19:42:23 -0800

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,6 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  validates :checkin, :checkout, presence: true 
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
-
+  validates :rating, :description, presence: true 
 end
