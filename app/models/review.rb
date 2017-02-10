6aba0813b6eb0a52e4ed97858f3ea315db0a8fcf
Jon Fougner <jon.fougner@gmail.com>
2017-02-10 21:22:27 +0000

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :reservation, presence: true
 end
