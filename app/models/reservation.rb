3ea4b5d97ee3feff9d89b9527569a3d2c22ecc7d
Jon Fougner <jon.fougner@gmail.com>
2017-02-10 21:58:49 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
+  belongs_to :host, :class_name => "User"
   has_one :review
 
   validates :checkin, presence: true
