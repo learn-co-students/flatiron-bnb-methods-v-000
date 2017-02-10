34c85f0a2d50b06e5155c70bf76be6ee896a0435
Jon Fougner <jon.fougner@gmail.com>
2017-02-10 20:57:16 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :no_shilling?
+
+  def no_shilling?
+    if guest_id == listing.host_id
+      errors.add(:guest_id, "cannot be the same user as the reservation’s listing’s host’s ID")
+    end
+  end
+
 end
