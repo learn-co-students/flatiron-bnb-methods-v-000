54cbb3680c4bc93d149490f0a199352ae99a1827
Jon Fougner <jon.fougner@gmail.com>
2017-02-10 21:10:01 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,7 @@ class Reservation < ActiveRecord::Base
   validates :checkin, presence: true
   validates :checkout, presence: true
   validate :no_shilling?
-  validate :no_conflicts?
+  validate :no_evictions?
   validate :no_time_traveling?
 
   def no_shilling?
@@ -15,7 +15,7 @@ class Reservation < ActiveRecord::Base
     end
   end
 
-  def no_conflicts?
+  def no_evictions?
     listing.reservations.each do |r|
       if checkin && checkout
       # The conditional above is to avoid erroring out when checkin or checkout is missing
@@ -27,10 +27,10 @@ class Reservation < ActiveRecord::Base
     end
   end
 
-  def :no_time_traveling?
+  def no_time_traveling?
     if checkin & checkout
       if checking.to_date > checkout.to_date
-        errors.add(:checkin, "cannot occur after checkout.")        
+        errors.add(:checkin, "cannot occur after checkout.")
       end
     end
   end
