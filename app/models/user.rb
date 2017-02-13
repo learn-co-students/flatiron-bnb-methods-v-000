e9926f56bd14d19be41e9ee61edb8eef2b40992c
github username pianistps <github email address pianistps@yahoo.com>
2017-02-13 12:35:40 -0600

    added the solution for host reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :guests, through: :reservations
+
+  #as a guest, knows about the hosts its had (FAILED - 1)
+  has_many :hosts, through: :reservations, source: "guest_id"
+  #as a host, knows about its reviews from guests (FAILED - 2)
+  has_many :host_reviews, through: :guests, source: "reviews"
 end
