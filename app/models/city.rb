3cf6341002975ea687a4991ee90b9841532944a0
Gordon Siu <gs2@GSMB2.local>
2017-02-14 23:57:45 -0800

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,20 +6,52 @@ class City < ActiveRecord::Base
 
 # binding.pry
 
-  def city_openings
-    openings=[]
-    # binding.pry
-    Reservation.all.each do |reservation|
-        city = reservation.listing.neighborhood.city
-        if self == city
+    def city_openings
+      openings=[]
+      # binding.pry
+      Reservation.all.each do |reservation|
+          city = reservation.listing.neighborhood.city
+          if self == city
 
-        end
+          end
+      end
     end
-  end
 
- def overlaps?(start_date, end_date, other)
-    (start_date - other.checkout) * (other.checkin - end_date) >= 0
- end
+   def overlaps?(start_date, end_date, other)
+      (start_date - other.checkout) * (other.checkin - end_date) >= 0
+   end
+
+   def self.highest_ratio_res_to_listings
+      listing_count = {}
+      ratios = {}
+      cities = self.reservation_count
+
+      City.all.each do |city|
+        listing_count[city] = 0
+          city.listings.each do |listing|
+            listing_count[city] += 1
+          end #listing
+          ratios[city] = cities[city] / listing_count[city]
+      end #city
+      ratios.max_by{|k,v| v}[0]
+   end
+
+   def self.most_res
+      city_count = []
+      Reservation.all.each do |reservation|
+        city_count << reservation.listing.neighborhood.city
+      end
+      freq = city_count.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+      city_count.max_by { |v| freq[v] }
+   end
+
+   def self.reservation_count
+      city_count = []
+      Reservation.all.each do |reservation|
+        city_count << reservation.listing.neighborhood.city
+      end
+      freq = city_count.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+   end
 
 end
 
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,39 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+    def self.most_res
+        neighborhood_count = []
+          Reservation.all.each do |reservation|
+            neighborhood_count << reservation.listing.neighborhood
+          end
+          freq = neighborhood_count.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+          neighborhood_count.max_by { |v| freq[v] }
+       end
+
+    def self.highest_ratio_res_to_listings
+        # binding.pry
+        listing_count = {}
+        ratios = {}
+        neighborhoods = self.reservation_count
+
+        Neighborhood.all.each do |neighborhood|
+           listing_count[neighborhood] = 0
+           neighborhood.listings.each do |listing|
+               listing_count[neighborhood] += 1
+           end #listing
+                if listing_count[neighborhood] > 0
+                  ratios[neighborhood] = neighborhoods[neighborhood] / listing_count[neighborhood]
+              end
+           end #neighborhood
+           ratios.max_by{|k,v| v}[0]
+    end
+
+   def self.reservation_count
+       neighborhood_count = []
+      Reservation.all.each do |reservation|
+         neighborhood_count << reservation.listing.neighborhood
+      end
+      freq =  neighborhood_count.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+   end
+
 end
