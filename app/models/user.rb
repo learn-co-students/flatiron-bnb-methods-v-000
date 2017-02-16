6202597215a226ca8b0cf82722adb94e5ad7e495
Jon Fougner <jon.fougner@gmail.com>
2017-02-16 04:56:57 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,4 @@ class User < ActiveRecord::Base
   has_many :guests, :foreign_key => 'host_id', :class_name => "User", :through => :listings
   has_many :guest_listings, :through => :trips, :source => :listing
   has_many :hosts, :through  => :guest_listings
-
-
-
-  # has_many :host_listings, :through => :trip_listings, :source => :listing
-  # has_many :hosts, :foreign_key => 'guest_id', :class_name => "User", :through => :host_listing
-
-
-  # has_many :listings, :foreign_key => 'guest_id', :class_name => "User", :through => :trips
-  # has_many :listings, :foreign_key => 'guest_id', :class_name => "User", :through => :reservations
-  # has_many :friendships
-  # has_many :hosts, :through => :friendships
 end
