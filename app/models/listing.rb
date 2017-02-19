65fad5be2a64ac56559caab433f198cb8b8f1998
John Verdone <jmverdone@gmail.com>
2017-02-19 13:27:41 -0500

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,40 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, presence: true
+  validates :listing_type, presence: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :neighborhood, presence: true
+
+  after_create :user_to_host
+  after_destroy :user_to_unhost
+
+
+  def average_review_rating
+    avg = 0.0
+    total = 0
+
+    self.reservations.each do |reservation|
+
+        total += reservation.review.rating
+        
+    end
+    avg = total/self.reservations.count.to_f
+  end
+
+  private
+
+    def user_to_host
+      # binding.pry
+      self.host.update(host: true)
+    end
+
+    def user_to_unhost
+      self.host.update(host: false) if self.host.listings.count == 0
+
+    end
+
 end
