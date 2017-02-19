15d08b12a3509185c1b779bdfc44cc33c06a215e
github username pianistps <github email address pianistps@yahoo.com>
2017-02-19 10:15:39 -0600

    Done.

--- app/models/concerns/available.rb
+++ app/models/concerns/available.rb
@@ -4,7 +4,7 @@ module Available
     def openings(start_date, end_date)
       listings.select do |listing|
         listing.reservations.each do |reservation|
-          ((reservation.checkin.to_date)..(reservation.checkout.to_date)) != ((start_date.to_date)..(end_date.to_date))
+          ((reservation.checkin.to_date)..(reservation.checkout.to_date)) == ((start_date.to_date)..(end_date.to_date))
         end
       end
     end
