50ded165b6ced8054bcf73f50d791f755ef6ac54
github username <github email address>
2017-02-24 20:47:11 -0500

    completed

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,11 @@
 class Neighborhood < ActiveRecord::Base
+  include Shareable::InstanceMethods
+  extend Shareable::ClassMethods
   belongs_to :city
   has_many :listings
+  has_many :reservations, through: :listings
 
+  def neighborhood_openings(first_date, second_date)
+    overlap(first_date, second_date)
+  end
 end
