a2cd3c6540b666fcc89f5ed44341c2fde72714ec
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-02-26 02:25:15 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,34 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
+  def city_openings(start, finish)
+    start = Date.parse(start)
+    finish = Date.parse(finish)
+
+    listings.select do |listing|
+    listing.reservations.none? do |reservation|
+    !((start >= reservation.checkout) || (finish <= reservation.checkin))
+    end
+   end
+  end
+  
+  def self.highest_ratio_res_to_listings
+    highest = City.all[0]
+    self.all.each do |city|
+      highest = city if (city.reservations.count/city.listings.count.to_f) > (highest.reservations.count/highest.listings.count.to_f)
+    end
+    highest
+  end
+
+
+
+  def self.most_res
+    most = City.all[0]
+    self.all.each do |city|
+      most = city if city.reservations.count > most.reservations.count
+    end
+    most
+  end
 end
-
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,32 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  has_many :reservations, :through => :listings
+
+  def neighborhood_openings(start, finish)
+    start = Date.parse(start)
+    finish = Date.parse(finish)
+
+    listings.select do |listing|
+    listing.reservations.none? do |reservation|
+    !((start >= reservation.checkout) || (finish <= reservation.checkin))
+    end
+   end
+  end
+  def self.highest_ratio_res_to_listings
+      highest = Neighborhood.all[0]
+      self.all.each do |neighborhood|
+        highest = neighborhood if (neighborhood.reservations.count/neighborhood.listings.count.to_f) > (highest.reservations.count/highest.listings.count.to_f)
+      end
+      highest
+    end
+
+  def self.most_res
+    most = Neighborhood.all[0]
+    self.all.each do |hood|
+      most = hood if hood.reservations.count > most.reservations.count
+    end
+    most
+  end
 
 end
