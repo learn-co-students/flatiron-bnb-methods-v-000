af936f1b21dd84d769061f9e4f47d9565fd47032
Anna Lansfjord <lansfjord@gmail.com>
2017-02-28 22:48:28 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,38 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :guest_is_not_host
+  validate :listing_available?
+  validate :checkin_precedes_checkout
+
+  def guest_is_not_host
+    if self.guest_id == self.listing.host_id
+      errors.add(:guest_is_host, "You cannot make a reservation on your own listing")
+    end
+  end
+
+  def listing_available?
+    if self.status != "accepted"
+      errors.add(:listing_not_available, "reservations can only be made for available listings")
+    end
+  end
+
+  def checkin_precedes_checkout
+    if self.checkin && self.checkout && self.checkin != self.checkout
+      self.checkin < self.checkout
+    else
+      errors.add(:checkin_does_not_precede_checkout, "checkin must precede checkout")
+    end
+  end
+
+  def duration
+    self.checkout - self.checkin
+  end
+
+  def total_price
+    (self.duration)*(self.listing.price)
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,15 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :reservation_id, presence: true
+  validate :checkout_passed
+
+  def checkout_passed
+    if self.reservation_id && Date.today < self.reservation.checkout
+      errors.add(:checkout_has_not_passed, "cannot write reviews until the checkout date has passed")
+    end
+  end
+
 end
