d0925f4ff9c36792311e9b7493b2ff0db4405420
Liah Wallace <liahwallace1@gmail.com>
2017-03-01 23:11:58 +0000

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,38 @@
 class Listing < ActiveRecord::Base
+
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+validates_presence_of :title, :description, :address, :listing_type, :price, :neighborhood
+
+before_create :user_to_host
+after_destroy :remove_host_status
+
+  def average_review_rating
+    ratings = self.reviews.collect {|review| review.rating}
+    ratings.reduce(:+).to_f / ratings.count
+  end
+
+  def dates_occupied
+    dates = self.reservations.collect do |res|
+      res.all_res_dates
+    end
+    dates.flatten!
+  end
+
+private
+
+  def user_to_host
+    self.host.update(host: true)
+  end
+
+  def remove_host_status
+    if self.host.listings.count == 0
+      self.host.update(host: false)
+    end
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,35 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates_presence_of :checkin, :checkout
+  validate :guest_is_not_host
+  validate :checkout_after_checkin
+  # validate :listing_available
+
+  def duration
+    self.all_res_dates.count
+  end
+
+  def total_price
+    self.price * self.duration
+  end
+
+  def all_res_dates
+    (self.checkin..self.checkout).collect {|day| day}
+  end
+
+  # def listing_available
+  #   self.all_res_dates.none? {|date| self.listing.dates_occupied.include?(date)}
+  # end
+
+private
+
+  def guest_is_not_host
+    guest_id != listing.host_id
+  end
+
+  def checkout_after_checkin
+    checkin < checkout
+  end
+
 end
