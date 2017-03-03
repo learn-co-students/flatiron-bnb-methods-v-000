ae9b7212128bc287bf0f028b85590e1ac484cfab
warrend <warrend555@gmail.com>
2017-03-03 13:04:27 -0500

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,49 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(checkin, checkout)
+  	openings = []	
+  	self.listings.each do |listing|
+  		if listing.available?(checkin, checkout)
+  			openings << listing
+  		end
+  	end
+  	openings
+  end
+
+  def self.highest_ratio_res_to_listings
+  	high = 0
+  	highest_hood = nil
+  	self.all.each do |hood|
+  		listings = hood.listings.count
+  		reservations = 0
+  		hood.listings.all.each do |listing|
+  			reservations += listing.reservations.count
+  		end
+  		if listings > 0
+  			ratio = reservations / listings
+  			if ratio > high 
+  				highest_hood = hood
+  				high = ratio 
+  			end
+  		end
+  	end
+  	highest_hood
+  end
+
+  def self.most_res
+  	highest_city = nil
+  	most_reservations = 0
+  	self.all.each do |city|
+  		city.listings.all.each do |listing|
+  			reservations = listing.reservations.count 
+  			if reservations > most_reservations
+  				most_reservations = reservations
+  				highest_city = city
+  			end
+  		end
+  	end
+  	highest_city
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,43 @@
 class Reservation < ActiveRecord::Base
+	validates_presence_of :checkin, :checkout
+	validate :own_listing
+	validate :check_availability
+	validate :checkout_after_checkin
+
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+
+  def duration
+  	(checkout - checkin).to_i
+  end
+
+  def total_price
+  	listing.price * duration
+  end
+
+
+  private 
+
+		def own_listing
+		  if self.guest == self.listing.host
+		    errors.add(:guest, "You can't reserve your own listing.")
+		  end
+		end
+
+		def check_availability
+	  	listing.reservations.each do |r|
+	  		dates_taken = (r.checkin..r.checkout)
+	  		if dates_taken === checkin || dates_taken === checkout
+	  			errors.add(:guest_id, "Unavailable.")
+	  		end
+	  	end
+  	end
+
+  	def checkout_after_checkin
+  		if checkin && checkout && checkout <= checkin
+  			errors.add(:guest_id, "Checkout must be after checkin")
+  		end
+  	end
 
 end
