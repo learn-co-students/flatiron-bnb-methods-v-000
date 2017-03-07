71cecd169dfe544bb8f9ed3ed4f4439928bd0a6e
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-07 09:21:43 +0000

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,18 +1,43 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
-  # def city_openings(checkin, checkout)
-  #   arr = []
-  #   self.listings.each do |listing|
-  #     if listing.reservations.available?(checkin, checkout)
-  #       arr << listing
-  #     end
-  #   end
-  #   arr
-  # end
 
-  def city_openings
+  def city_openings(checkin, checkout)
+    arr = []
+    listings.each do |listing|
+      listing.reservations.each do |reservation|
+        if (Date.parse(checkin) <= reservation.checkout) && (Date.parse(checkout) >= reservation.checkin)
+          arr << listing
+        end
+      end
+    end
+    arr
   end
+
+  def self.most_res
+    most_res = City.first
+    City.all.each do |city|
+      if most_res.reservations.length < city.reservations.length
+         most_res = city
+       end
+     end
+    most_res
+  end
+
+  def self.highest_ratio_res_to_listings
+    store = City.first
+    City.all.each do |city|
+      if (store.reservations.length / store.listings.length) < (city.reservations.length / city.listings.length)
+         store = city
+       end
+     end
+    store
+  end
+
+
+
+
 
 end
