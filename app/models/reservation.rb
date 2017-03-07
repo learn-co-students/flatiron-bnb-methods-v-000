a9ca228c63a1d934c3cdd20be5b1a30947b365d0
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-03-07 04:10:06 +0000

    Done.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,10 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  def available?(start_date, end_date)
+    start_date1 = Date.parse(start_date)
+    end_date1 = Date.parse(end_date)
+    self.checkin >= start_date1 && self.checkout <= end_date1
+  end
+
 end
