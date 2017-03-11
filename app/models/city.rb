1ef7e296c60fbca5192524b848336a43149abb87
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-11 16:27:49 +0100

    working version of city_openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,70 +4,12 @@ class City < ActiveRecord::Base
   has_many :reservations, :through => :listings
 
   def city_openings(start_date, end_date)
+    start_date_parsed = Date.parse(start_date)
+    end_date_parsed   = Date.parse(end_date)
 
-    self.listings.merge(Listing.available(start_date, end_date))
-    # self.listings.select{ |listing| listing.available(start_date, end_date) }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # Listing.joins(:neighborhood).joins("LEFT OUTER JOIN reservations ON listings.id = reservations.listing_id").where(neighborhoods: {city_id: self.id}, reservations: {id: nil})+Listing.joins(:neighborhood).joins(:reservations).where("neighborhoods.city_id = ?", self.id).where.not("reservations.checkout >= ? AND ? >= reservations.checkin", start_date, end_date)
-
-    # Listing.joins(:neighborhood).joins("LEFT OUTER JOIN reservations ON listings.id = reservations.listing_id").where(neighborhoods: {city_id: 1}, reservations: {id: nil})+Listing.joins(:neighborhood).joins(:reservations).where("neighborhoods.city_id = ? AND NOT ? BETWEEN reservations.checkin AND reservations.checkout AND NOT ? BETWEEN reservations.checkin AND reservations.checkout", 1, '2014-05-01', '2014-05-05').group("listings.id")
-
-  	# self.listings.merge( Listing.bookable(start_date, end_date) )
-  	# intersect the city's (neighborhood's) listings with bookable listings
-  	# City.first.city_openings('2014-05-01', '2014-05-05')
-  	# will SQL query
-  	#
-  	# v1
-  	# SELECT "listings".* FROM "listings" 
-  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
-  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
-  	# 	WHERE "neighborhoods"."city_id" = ? 
-  	# 	AND (NOT (checkin <= '2014-05-05' AND checkout >= '2014-05-01'))
-  	#   [["city_id", 1]]
-  	#
-  	# v2
-  	# SELECT "listings".* FROM "listings" 
-  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
-  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
-  	# 	WHERE "neighborhoods"."city_id" = ? 
-  	# 	AND (NOT ("reservations"."checkin" BETWEEN '2014-05-01' AND '2014-05-05')) 
-  	# 	AND (NOT ("reservations"."checkout" BETWEEN '2014-05-01' AND '2014-05-05'))  
-  	# 	[["city_id", 1]]
-  	#
-  	# v3
-  	# SELECT "listings".* FROM "listings" 
-  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
-  	# 	WHERE (NOT ("reservations"."checkin" BETWEEN '2014-05-01' AND '2014-05-05'))
-  	# SELECT "listings".* FROM "listings" 
-  	# 	INNER JOIN "reservations" ON "reservations"."listing_id" = "listings"."id" 
-  	# 	WHERE (NOT ("reservations"."checkout" BETWEEN '2014-05-01' AND '2014-05-05'))
-  	# SELECT "listings".* FROM "listings" 
-  	# 	INNER JOIN "neighborhoods" ON "listings"."neighborhood_id" = "neighborhoods"."id" 
-  	# 	WHERE "neighborhoods"."city_id" = ?  
-  	# 	[["city_id", 1]]		
+    self.listings.collect do |listing|
+      listing if listing.reservations.none?{ |reservation| (reservation.checkin <= end_date_parsed) && (reservation.checkout >= start_date_parsed) }
+    end
   end
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,41 +4,5 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
 
-  private
-  
-  def self.available(start_date, end_date)
-  	period = start_date..end_date
-  	if start_date && end_date
-  		no_checkin_in_period 	= self.joins(:reservations).where.not(reservations: {checkin: period})
-  		no_checkout_in_period 	= self.joins(:reservations).where.not(reservations: {checkout: period})
-  		no_checkin_in_period.merge(no_checkout_in_period)
-  		# merge acts like AND
-  	else
-  		[]
-  	end
-  end
-
-
-
- #  def self.bookable(start_date, end_date)
- #  # v3 was
-	# # if start_date && end_date
- #  #  	joins(:reservations).where.not(reservations: {checkin: start_date..end_date}) +
- #  #   joins(:reservations).where.not(reservations: {checkout: start_date..end_date})
- #  # else
- #  #   []
- #  # end
- #  # 
-	# # v2 was
- # 	# period = start_date..end_date
-	# # no_checkin_in_period = Reservation.where.not(checkin: period)
-	# # no_checkout_in_period = Reservation.where.not(checkout: period)
-	# # self.joins(:reservations).merge(no_checkin_in_period).merge(no_checkout_in_period)
-	# #
-	# # v1 was
-	# # self.joins(:reservations).merge( Reservation.where.not("checkin <= ? AND checkout >= ?", end_date, start_date) )
-	# # listings that have reservations, AND these reservations are not overlapping with the start and end date
- #  end
 end
