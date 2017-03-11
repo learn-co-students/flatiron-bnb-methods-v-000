d3b82ae15b1a2517d4c09f2c6f481a0e947551bf
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-11 16:32:22 +0100

    refactored city_openings with an #available? method and select block

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,12 +4,8 @@ class City < ActiveRecord::Base
   has_many :reservations, :through => :listings
 
   def city_openings(start_date, end_date)
-    start_date_parsed = Date.parse(start_date)
-    end_date_parsed   = Date.parse(end_date)
-
-    self.listings.collect do |listing|
-      listing if listing.reservations.none?{ |reservation| (reservation.checkin <= end_date_parsed) && (reservation.checkout >= start_date_parsed) }
-    end
+    self.listings.select{ |listing| listing.available?(start_date, end_date) }
   end
+  
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,14 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
 
+  def available?(start_date, end_date)
+    start_date_parsed = Date.parse(start_date)
+    end_date_parsed   = Date.parse(end_date)
+
+    # a listing is available only if NONE of the associated reservations is on the queried time
+  	self.reservations.none? do |reservation| 
+  		(reservation.checkin <= end_date_parsed) && (reservation.checkout >= start_date_parsed)
+  	end
+  end
+
 end
