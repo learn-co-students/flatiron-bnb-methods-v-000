c13047b8d54ea10fa0e72422972fa9cd6d7a5165
jminterwebs <jmverdone@gmail.com>
2017-03-25 18:00:26 -0400

    Update city.rb

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,17 +7,11 @@ class City < ActiveRecord::Base
 
   def city_openings(start_date, end_date)
 
-    if start_date.class == Date
-      begin_date = start_date
-    else
-       begin_date = DateTime.strptime(start_date, "%Y-%m-%d")
-    end
-
-    if end_date.class == Date
-      date_end = end_date
-    else
+   
+      begin_date = DateTime.strptime(start_date, "%Y-%m-%d")
+   
       date_end = DateTime.strptime(end_date, "%Y-%m-%d")
-    end
+   
 
     search_range = (begin_date .. date_end)
     city_openings = []
