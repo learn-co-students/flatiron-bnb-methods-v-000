5d6f95a662a9d8f85470a064150d7c77679d3faa
Callie Brazil <cmabrazil@gmail.com>
2017-03-27 01:07:56 +0000

    user model complete

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,8 @@
 class Reservation < ActiveRecord::Base
   belongs_to :listing
-  belongs_to :guest, :class_name => "User"
+  belongs_to :guest, class_name: "User"
   has_one :review
+  has_one :host, through: :listing
 
   validates :checkin, presence: true
   validates :checkout, presence: true
