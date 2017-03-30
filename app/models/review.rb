ff314a2d2e282ea582d19ab0254ce014c92bb360
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-30 10:27:24 -0400

    Passing review spec

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,16 @@
 class Review < ActiveRecord::Base
-  belongs_to :reservation
-  belongs_to :guest, :class_name => "User"
+    belongs_to :reservation
+    belongs_to :guest, :class_name => "User"
 
+    validates :rating, presence:true
+    validates :description, presence:true
+    validate :comes_after_valid_checkout
+    
+    private
+    def comes_after_valid_checkout
+        if !(reservation && reservation.status == "accepted" && reservation.checkout < Date.today)
+            errors.add(:review, "must apply to an accepted reservation that has passed") 
+        end
+    end
+    
 end
