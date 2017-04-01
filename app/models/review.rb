36310f927bb868dc63199a9dc309d12e740c5b93
Christina L. Cole <christinalcole@gmail.com>
2017-04-01 14:40:48 -0400

    add assoc, validations to pass master, soln specs

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,17 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
+  validates :description, :rating, :reservation_id, presence: true
+  validates :rating, numericality: true
+
+  before_validation :is_trip_completed
+
+  private
+  def is_trip_completed
+    (reservation) && (reservation.status == "accepted") && (reservation.checkout < Date.today)
+  end
+
 end
+
+
+## Passes both master and solution branch specs
