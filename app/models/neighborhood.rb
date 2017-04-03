543c38ba3ea2fb58afb986f68b3f563700936c13
Christina L. Cole <christinalcole@gmail.com>
2017-04-02 21:59:40 -0400

    abstract common methods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,27 +3,11 @@ class Neighborhood < ActiveRecord::Base
   has_many :listings
   has_many :reservations, through: :listings
 
+  extend Reservable::ClassMethods
+  include Reservable::InstanceMethods
+
   def neighborhood_openings(checkin, checkout)
-    listings.find_all do |listing|
-      listing.reservations.all? do |res|
-         (checkin.to_datetime >= res.checkout) || (checkout.to_datetime <= res.checkin)
-      end
-    end
+    openings(checkin, checkout)
   end
 
-  def res_to_listings_ratio
-    if listings.count > 0
-      reservations.count.to_f / listings.count.to_f
-    else
-      0
-    end
-  end
-
-  def self.highest_ratio_reservations_to_listings
-    self.all.max {|a, b| a.res_to_listings_ratio <=> b.res_to_listings_ratio}
-  end
-
-  def self.most_reservations
-    self.all.max {|a, b| a.reservations.count <=> b.reservations.count}
-  end
 end
