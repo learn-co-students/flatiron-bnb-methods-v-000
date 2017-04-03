28a572120472db591daf6041c168a9f6d5f7596f
Christina L. Cole <christinalcole@gmail.com>
2017-04-02 21:23:24 -0400

    initial commit, passing master

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,29 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  has_many :reservations, through: :listings
 
+  def neighborhood_openings(checkin, checkout)
+    listings.find_all do |listing|
+      listing.reservations.all? do |res|
+         (checkin.to_datetime >= res.checkout) || (checkout.to_datetime <= res.checkin)
+      end
+    end
+  end
+
+  def res_to_listings_ratio
+    if listings.count > 0
+      reservations.count.to_f / listings.count.to_f
+    else
+      0
+    end
+  end
+
+  def self.highest_ratio_reservations_to_listings
+    self.all.max {|a, b| a.res_to_listings_ratio <=> b.res_to_listings_ratio}
+  end
+
+  def self.most_reservations
+    self.all.max {|a, b| a.reservations.count <=> b.reservations.count}
+  end
 end
