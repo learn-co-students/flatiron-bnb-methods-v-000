95e917ed4726d6e2461fb563322787dcd0854fb5
bentonwong <bentonwong@gmail.com>
2017-04-11 23:54:24 -0700

    add solution

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,31 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :listing_type, :title, :description, :price, :neighborhood_id, presence: true
+
+  after_create :change_host_status
+  after_destroy :change_host_status_if_no_other_listing
+
+  def average_review_rating
+    all_reservations_ids = Reservation.where("listing_id = ?",self.id).collect{|reservation| reservation.id}
+    ratings = all_reservations_ids.collect{|reservation_id| Review.find_by_reservation_id(reservation_id).rating}
+    ratings.sum.to_f/ratings.count.to_f
+  end
+
+  protected
+
+    def change_host_status
+      host = User.find_by_id(self.host_id)
+      host.host = !host.host
+      host.save
+    end
+
+    def change_host_status_if_no_other_listing
+      if Listing.where("host_id = ?",self.host_id).empty? && User.find_by_id(self.host_id).host
+        change_host_status
+      end
+    end
+
+
 end
--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -2,4 +2,50 @@ class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
 
+  def neighborhood_openings(start_date, end_date)
+    all_listings_ids = Listing.all.collect{|listing| listing.id} #returns list of all listing_ids
+
+    overlapping_listing_ids = Reservation.all.select{|reservation| (reservation.checkin..reservation.checkout).overlaps?(start_date.to_date..end_date.to_date)}.collect{|result| result.listing_id}.uniq{|result| result} #identities overlapping instances and returns their listing_ids
+
+    desired_listing_ids = all_listings_ids - overlapping_listing_ids #excludes listing_ids that overlap from all listing_ids
+    desired_listing_ids.collect{|listing_id| Listing.find(listing_id)}
+  end
+
+  def self.highest_ratio_res_to_listings
+    all_neighborhood_ids = []
+    neighborhood_id_highest_ratio = 0
+    listings_by_neighborhood_id = Hash.new(0)
+    count_of_listings_by_neighborhood_id = Hash.new(0)
+    count_of_reservations_by_neighborhood_id = Hash.new(0)
+    ratio_reservation_to_listings = Hash.new(0)
+
+    all_neighborhood_ids = Neighborhood.all.collect{|neighborhood| neighborhood.id}
+
+    all_neighborhood_ids.each do |neighborhood_id|
+      listings_by_neighborhood_id[neighborhood_id] = Listing.where("neighborhood_id = ?",neighborhood_id)
+
+      count_of_listings_by_neighborhood_id[neighborhood_id] = listings_by_neighborhood_id[neighborhood_id].count
+
+      count_of_reservations_by_neighborhood_id[neighborhood_id] = listings_by_neighborhood_id[neighborhood_id].collect{|listing| Reservation.where("listing_id = ?",listing.id).count}.sum
+
+      if count_of_listings_by_neighborhood_id[neighborhood_id] != 0
+        ratio_reservation_to_listings[neighborhood_id] = count_of_reservations_by_neighborhood_id[neighborhood_id].to_f/count_of_listings_by_neighborhood_id[neighborhood_id].to_f
+      end
+    end
+    neighborhood_id_highest_ratio = ratio_reservation_to_listings.max_by{|key,value| value}.first
+    Neighborhood.find(neighborhood_id_highest_ratio)
+  end
+
+  def self.most_res
+    all_neighborhood_ids = Neighborhood.all.collect{|neighborhood| neighborhood.id}
+    count_of_reservations_by_neighborhood_id = Hash.new(0)
+    listings_by_neighborhood_id = Hash.new(0)
+    all_neighborhood_ids.each do |neighborhood_id|
+      listings_by_neighborhood_id[neighborhood_id] = Listing.where("neighborhood_id = ?", neighborhood_id)
+      count_of_reservations_by_neighborhood_id[neighborhood_id] = listings_by_neighborhood_id[neighborhood_id].collect{|listing| Reservation.where("listing_id = ?",listing.id).count}.sum
+    end
+    neighborhood_id_most_res = count_of_reservations_by_neighborhood_id.max_by{|key,value| value}.first
+    Neighborhood.find_by_id(neighborhood_id_most_res)
+  end
+
 end
