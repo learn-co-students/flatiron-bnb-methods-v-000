35a914bba7f4a60d74c81d5cda415f864aa20b72
Howard DeVennish <howarddevennish@gmail.com>
2017-04-24 18:47:23 +0000

    update listings validations and callbacks

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,5 +11,21 @@ class Listing < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
+  validates :neighborhood, presence: true
+
+  before_create :make_user_host_status_true
+  before_destroy :make_user_host_status_false
+
+
+
+  private
+
+  def make_user_host_status_true
+    host.update(host: true) unless host.host == true
+  end
+
+  def make_user_host_status_false
+    host.update(host: false) if host.listings.count == 0
+  end
 
 end
