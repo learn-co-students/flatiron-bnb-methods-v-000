cbbd753fa02eeba2c97493e5ab7b3544bc69366c
Howard DeVennish <howarddevennish@gmail.com>
2017-04-24 17:07:02 +0000

    fix city_openings method, add listing and reservation count

--- app/models/city.rb
+++ app/models/city.rb
@@ -8,10 +8,21 @@ class City < ActiveRecord::Base
     self.listings.each do |listing|
       listing.reservations.each do |res|
         if (Date.parse(checkin) > res.checkin && Date.parse(checkin) < res.checkout) || (Date.parse(checkout) > res.checkin && Date.parse(checkout) < res.checkout)
-          available.delete(listing)
+          availables.delete(listing)
         end
       end
     end
+    availables
+  end
+
+  def listings_count
+    self.listings.count
+  end
+
+  def res_count
+    count = 0
+    self.listings.each {|res| count += 1}
+    count
   end
 
 end
