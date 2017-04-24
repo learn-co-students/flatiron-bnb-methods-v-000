a26d848628a1e89dc287e69443d2f3c020b1b3ee
Howard DeVennish <howarddevennish@gmail.com>
2017-04-24 16:25:08 +0000

    attempt city openings

--- app/models/city.rb
+++ app/models/city.rb
@@ -4,7 +4,14 @@ class City < ActiveRecord::Base
 
 
   def city_openings(checkin, checkout)
-
+    availables = self.listings.collect{|a| a}
+    self.listings.each do |listing|
+      listing.reservations.each do |res|
+        if (Date.parse(checkin) > res.checkin && Date.parse(checkin) < res.checkout) || (Date.parse(checkout) > res.checkin && Date.parse(checkout) < res.checkout)
+          available.delete(listing)
+        end
+      end
+    end
   end
 
 end
