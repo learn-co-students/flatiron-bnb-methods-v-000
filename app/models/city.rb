8e66fa6c208b80553d2f1c5d3fc00e30a838abba
Seth <itz.saga@gmail.com>
2017-04-28 22:59:29 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,13 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    self.listings.map do |listing|
+      if start_date.to_i <= listing.created_at.to_i && listing.created_at.to_i <= end_date.to_i
+        listing
+      else
+        nil
+      end
+    end
+  end
 end
-
