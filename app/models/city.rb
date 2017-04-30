cae65e51231a61fbc4bc5d3e1b35c214a5eb4c26
kkashuda <kkashuda@gmail.com>
2017-04-29 21:56:57 -0500

    Done.

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,30 @@
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings 
+
+  def city_openings(start_date, end_date)
+    openings = [] 
+    start_date = Date.parse(start_date)
+    end_date = Date.parse(end_date)
+
+    listings.each do |l| 
+       openings << l if !l.reservations.any? {|reservation| reservation.checkin <= end_date && reservation.checkout >= start_date }
+    end 
+    openings 
+  end 
+
+  def self.highest_ratio_res_to_listings
+    cities = City.all.sort_by {|c| c.reservations.count.to_f / c.listings.count.to_f}
+    highest_ratio = cities.last 
+  end 
+
+  def self.most_res 
+    cities = City.all.sort_by {|c| c.reservations.count}
+    highest = cities.last 
+  end 
+  
+  
 
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,17 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  validates :rating, presence: true 
+  validates :description, presence: true 
+  validates :reservation_id, presence: true
+  validate :accepted_and_checked_out
+
+  def accepted_and_checked_out
+    if reservation == nil || reservation.status != "accepted" || reservation.checkout > Date.today
+      errors.add(:accepted_and_checked_out, "Sorry, invalid entry.")
+    end 
+  end 
+  
+
 
 end
