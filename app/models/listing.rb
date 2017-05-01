464a2116f75630854701dc99be678a67ef6f482f
anytimesoon <damien@delartigue.co.uk>
2017-05-01 16:13:25 +0100

    listing callback methods passing tests

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,20 @@ class Listing < ActiveRecord::Base
   has_many :guests, :class_name => "User", :through => :reservations
   
   validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+
+  before_save :host_status_toggle 
+  before_destroy :host_has_listings
+
+  def host_status_toggle
+  	self.host.host = true
+  	self.host.save
+  end
+
+  def host_has_listings
+  	if self.host.listings.length == 1
+  		  	self.host.host = false
+  	self.host.save
+  	end
+  end
+
 end
