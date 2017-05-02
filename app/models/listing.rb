b6471eb7e9e16ae9ad8991911a4d713831c61831
Chirag Dudhat <cdudhat@gmail.com>
2017-05-01 20:25:27 -0400

    Done.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,14 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  validates :address, :listing_type, :title, :description, :price, :neighborhood, presence: true
+  after_create :change_host_status
+
+  private
+
+  def change_host_status
+    self.host.host = true
+    self.host.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
-  
+
+  #before_save :change_host_status_back
+
+  private
+
+  #def change_host_status_back
+#    self.host = false if !self.listings.any?
+#  end
 end
