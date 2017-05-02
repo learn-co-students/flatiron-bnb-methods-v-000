f1d5de8327c6803e956e62861f64a9e86e3815f5
Chenxi Wu <wuchenxi2013@gmail.com>
2017-05-02 19:11:22 +0200

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
-
+  validates :rating, presence: true
+  validates :description, presence: true
+  validates :reservation, presence: true
+  validate :val
+  def val
+    if reservation!=nil and reservation.status=='pending' then errors.add(:reservation, "not accepted") end
+    if reservation!=nil and reservation.checkout>Date.today then errors.add(:reservation, "not checked out yet") end
+  end
 end
