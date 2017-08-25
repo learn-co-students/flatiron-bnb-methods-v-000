0687a01e797ee19c7de478d53102409d30b081f3
jinstrider2000 <efrainperezjr@live.com>
2017-08-25 00:06:16 -0400

    added method defs to city class

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,16 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def self.highest_ratio_res_to_listings
+    
+  end
+
+  def self.most_res
+    
+  end
+
+  def city_openings
+    
+  end
 end
 
